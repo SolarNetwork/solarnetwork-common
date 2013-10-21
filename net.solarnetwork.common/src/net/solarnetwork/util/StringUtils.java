806be5964bb42dbaa1d8a29cad9166a55ac986e6
Matt Magoffin <git+matt@msqr.us>
2013-10-22 08:54:32 +1300

    Fix for possible NPE; instead default to sensible values.

--- net.solarnetwork.common/src/net/solarnetwork/util/StringUtils.java
+++ net.solarnetwork.common/src/net/solarnetwork/util/StringUtils.java
@@ -18,8 +18,6 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 
  * 02111-1307 USA
  * ==================================================================
- * $Id$
- * ==================================================================
  */
 
 package net.solarnetwork.util;
@@ -35,37 +33,48 @@ import java.util.regex.Pattern;
  * Common string helper utilities.
  * 
  * @author matt
- * @version $Revision$
+ * @version 1.1
  */
 public final class StringUtils {
 
 	private StringUtils() {
 		// don't construct me
 	}
-	
+
 	/**
 	 * Get a comma-delimited string from a collection of objects.
 	 * 
-	 * @param set the set
+	 * @param set
+	 *        the set
 	 * @return the comma-delimited string
 	 * @see #delimitedStringFromCollection(Set, String)
 	 */
 	public static String commaDelimitedStringFromCollection(final Collection<?> set) {
 		return delimitedStringFromCollection(set, ",");
 	}
-	
+
 	/**
 	 * Get a delimited string from a collection of objects.
 	 * 
-	 * <p>This will call the {@link Object#toString()} method on each object
-	 * in the set, using the natural iteration ordering of the set.No attempt 
-	 * to escape delimiters within the set's values is done.</p>
+	 * <p>
+	 * This will call the {@link Object#toString()} method on each object in the
+	 * set, using the natural iteration ordering of the set.No attempt to escape
+	 * delimiters within the set's values is done.
+	 * </p>
 	 * 
-	 * @param set the set
-	 * @param delim the delimiter
+	 * @param set
+	 *        the set
+	 * @param delim
+	 *        the delimiter
 	 * @return the delimited string
 	 */
 	public static String delimitedStringFromCollection(final Collection<?> set, String delim) {
+		if ( set == null ) {
+			return null;
+		}
+		if ( delim == null ) {
+			delim = "";
+		}
 		StringBuilder buf = new StringBuilder();
 		for ( Object o : set ) {
 			if ( buf.length() > 0 ) {
@@ -81,25 +90,32 @@ public final class StringUtils {
 	/**
 	 * Get a Set via a comma-delimited string value.
 	 * 
-	 * @param list the comma-delimited string
-	 * @return the Set, or <em>null</em> if {@code list} is <em>null</em> or an empty string
+	 * @param list
+	 *        the comma-delimited string
+	 * @return the Set, or <em>null</em> if {@code list} is <em>null</em> or an
+	 *         empty string
 	 * @see #delimitedStringToSet(String, String)
 	 */
 	public static Set<String> commaDelimitedStringToSet(final String list) {
 		return delimitedStringToSet(list, ",");
 	}
-	
+
 	/**
 	 * Get a string Set via a delimited String value.
 	 * 
-	 * <p>The format of the {@code list} String should be a delimited
-	 * list of values. Whitespace is permitted around the delimiter, and will be
+	 * <p>
+	 * The format of the {@code list} String should be a delimited list of
+	 * values. Whitespace is permitted around the delimiter, and will be
 	 * stripped from the values. Whitespace is also trimmed from the start and
-	 * end of the input string.</p>
+	 * end of the input string.
+	 * </p>
 	 * 
-	 * @param list the delimited text
-	 * @param delim the delimiter to split the list with
-	 * @return the Set, or <em>null</em> if {@code list} is <em>null</em> or an empty string
+	 * @param list
+	 *        the delimited text
+	 * @param delim
+	 *        the delimiter to split the list with
+	 * @return the Set, or <em>null</em> if {@code list} is <em>null</em> or an
+	 *         empty string
 	 */
 	public static Set<String> delimitedStringToSet(final String list, final String delim) {
 		if ( list == null || list.length() < 1 ) {
@@ -112,45 +128,59 @@ public final class StringUtils {
 		}
 		return s;
 	}
-	
+
 	/**
 	 * Get string Map via a comma-delimited String value.
 	 * 
-	 * <p>The format of the {@code mapping} String should be:</p>
+	 * <p>
+	 * The format of the {@code mapping} String should be:
+	 * </p>
 	 * 
-	 * <pre>key=val[,key=val,...]</pre>
+	 * <pre>
+	 * key=val[,key=val,...]
+	 * </pre>
 	 * 
-	 * @param mapping the delimited text
+	 * @param mapping
+	 *        the delimited text
 	 * @see #delimitedStringToMap(String, String, String)
 	 */
 	public static Map<String, String> commaDelimitedStringToMap(final String mapping) {
 		return delimitedStringToMap(mapping, ",", "=");
 	}
-	
+
 	/**
 	 * Get a string Map via a delimited String value.
 	 * 
-	 * <p>The format of the {@code mapping} String should be:</p>
+	 * <p>
+	 * The format of the {@code mapping} String should be:
+	 * </p>
 	 * 
-	 * <pre>key=val[,key=val,...]</pre>
+	 * <pre>
+	 * key=val[,key=val,...]
+	 * </pre>
 	 * 
-	 * <p>The record and field delimiters are passed as parameters to this method.
-	 * Whitespace is permitted around all delimiters, and will be stripped
-	 * from the keys and values. Whitespace is also trimmed from the start and
-	 * end of the input string.</p>
+	 * <p>
+	 * The record and field delimiters are passed as parameters to this method.
+	 * Whitespace is permitted around all delimiters, and will be stripped from
+	 * the keys and values. Whitespace is also trimmed from the start and end of
+	 * the input string.
+	 * </p>
 	 * 
-	 * @param mapping the delimited text
-	 * @param recordDelim the key+value record delimiter
-	 * @param fieldDelim the key+value delimiter
+	 * @param mapping
+	 *        the delimited text
+	 * @param recordDelim
+	 *        the key+value record delimiter
+	 * @param fieldDelim
+	 *        the key+value delimiter
 	 */
-	public static Map<String, String> delimitedStringToMap(final String mapping, 
+	public static Map<String, String> delimitedStringToMap(final String mapping,
 			final String recordDelim, final String fieldDelim) {
 		if ( mapping == null || mapping.length() < 1 ) {
 			return null;
 		}
-		final String[] pairs = mapping.trim().split("\\s*" +Pattern.quote(recordDelim) +"\\s*");
+		final String[] pairs = mapping.trim().split("\\s*" + Pattern.quote(recordDelim) + "\\s*");
 		final Map<String, String> map = new LinkedHashMap<String, String>();
-		final Pattern fieldSplit = Pattern.compile("\\s*" + Pattern.quote(fieldDelim) +"\\s*");
+		final Pattern fieldSplit = Pattern.compile("\\s*" + Pattern.quote(fieldDelim) + "\\s*");
 		for ( String pair : pairs ) {
 			String[] kv = fieldSplit.split(pair);
 			if ( kv == null || kv.length != 2 ) {
