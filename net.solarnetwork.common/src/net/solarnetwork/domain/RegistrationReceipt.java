b188d5b4d4f59681fe1543cc3257626515c5da2f
Matt Magoffin <git+matt@msqr.us>
2015-05-01 14:28:13 +1200

    NET-30: add support for URL-encoded email address value, to ease integration with registration process and fix URL encoding issues.

--- net.solarnetwork.common/src/net/solarnetwork/domain/RegistrationReceipt.java
+++ net.solarnetwork.common/src/net/solarnetwork/domain/RegistrationReceipt.java
@@ -30,20 +30,27 @@ import java.io.Serializable;
  * A receipt for registration.
  * 
  * @author matt
- * @version $Id$
+ * @version 1.1
  */
 public interface RegistrationReceipt extends Serializable {
-	
+
 	/**
 	 * Get the username that has been registered.
 	 * 
 	 * @return the email address
 	 */
 	String getUsername();
-	
+
 	/**
-	 * Get the confirmation code required to activate the registered
-	 * user.
+	 * Get the uesrname that has been registered, encoded as a URL component.
+	 * 
+	 * @return the uesrname, URL encoded
+	 * @since 1.1
+	 */
+	String getUsernameURLComponent();
+
+	/**
+	 * Get the confirmation code required to activate the registered user.
 	 * 
 	 * @return confirmation code
 	 */
