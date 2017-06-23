bb53f9c49b67b93ee475b518bb7e6d35ef3c006f
Matt Magoffin <matt@solarnetwork.net>
2017-06-23 18:01:21 +1200

    Pull in JsonUtils from SolarNet, as it is more generally useful.

--- net.solarnetwork.common/src/net/solarnetwork/util/JsonUtils.java
+++ net.solarnetwork.common/src/net/solarnetwork/util/JsonUtils.java
@@ -1,7 +1,7 @@
 /* ==================================================================
- * JsonNodeUtils.java - 7/04/2017 8:31:02 PM
+ * JsonUtils.java - 15/05/2015 11:46:24 am
  * 
- * Copyright 2017 SolarNetwork.net Dev Team
+ * Copyright 2007 SolarNetwork.net Dev Team
  * 
  * This program is free software; you can redistribute it and/or 
  * modify it under the terms of the GNU General Public License as 
@@ -22,27 +22,196 @@
 
 package net.solarnetwork.util;
 
+import java.io.IOException;
 import java.math.BigDecimal;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Set;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import net.solarnetwork.domain.GeneralDatumMetadata;
 
 /**
- * Utilities for parsing values from {@link JsonNode} objects.
+ * Utilities for JSON data.
  * 
  * @author matt
- * @version 1.1
- * @since 1.35
- * @deprecated use {@link JsonUtils} instead
+ * @version 1.0
+ * @since 1.36
  */
-@Deprecated
-public final class JsonNodeUtils {
+public final class JsonUtils {
 
-	private static final Logger LOG = LoggerFactory.getLogger(JsonNodeUtils.class);
+	/** A type reference for a Map with string keys. */
+	public static final TypeReference<LinkedHashMap<String, Object>> STRING_MAP_TYPE = new StringMapTypeReference();
+
+	private static final Logger LOG = LoggerFactory.getLogger(JsonUtils.class);
+
+	private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper()
+			.setSerializationInclusion(Include.NON_NULL)
+			.configure(DeserializationFeature.USE_BIG_DECIMAL_FOR_FLOATS, true);
+
+	private static final class StringMapTypeReference
+			extends TypeReference<LinkedHashMap<String, Object>> {
+
+		public StringMapTypeReference() {
+			super();
+		}
+
+	}
+
+	// don't construct me
+	private JsonUtils() {
+		super();
+	}
+
+	/**
+	 * Convert an object to a JSON string. This is designed for simple values.
+	 * An internal {@link ObjectMapper} will be used, and null values will not
+	 * be included in the output. All exceptions while serializing the object
+	 * are caught and ignored.
+	 * 
+	 * @param o
+	 *        the object to serialize to JSON
+	 * @param defaultValue
+	 *        a default value to use if {@code o} is <em>null</em> or if any
+	 *        error occurs serializing the object to JSON
+	 * @return the JSON string
+	 */
+	public static String getJSONString(final Object o, final String defaultValue) {
+		String result = defaultValue;
+		if ( o != null ) {
+			try {
+				return OBJECT_MAPPER.writeValueAsString(o);
+			} catch ( Exception e ) {
+				LOG.error("Exception marshalling {} to JSON", o, e);
+			}
+		}
+		return result;
+	}
+
+	/**
+	 * Convert a JSON string to an object. This is designed for simple values.
+	 * An internal {@link ObjectMapper} will be used, and all floating point
+	 * values will be converted to {@link BigDecimal} values to faithfully
+	 * represent the data. All exceptions while deserializing the object are
+	 * caught and ignored.
+	 * 
+	 * @param json
+	 *        the JSON string to convert
+	 * @param clazz
+	 *        the type of Object to map the JSON into
+	 * @return the object
+	 * @since 1.1
+	 */
+	public static <T> T getObjectFromJSON(final String json, Class<T> clazz) {
+		T result = null;
+		if ( json != null ) {
+			try {
+				result = OBJECT_MAPPER.readValue(json, clazz);
+			} catch ( Exception e ) {
+				LOG.error("Exception deserialzing json {}", json, e);
+			}
+		}
+		return result;
+	}
+
+	/**
+	 * Convert a JSON string to a Map with string keys.
+	 * 
+	 * <p>
+	 * This is designed for simple values. An internal {@link ObjectMapper} will
+	 * be used, and all floating point values will be converted to
+	 * {@link BigDecimal} values to faithfully represent the data. All
+	 * exceptions while deserializing the object are caught and ignored.
+	 * </p>
+	 * 
+	 * @param json
+	 *        the JSON to convert
+	 * @return the map, or {@literal null} if {@code json} is {@literal null} or
+	 *         empty, or any exception occurs generating the JSON
+	 */
+	public static Map<String, Object> getStringMap(final String json) {
+		if ( json == null || json.length() < 1 ) {
+			return null;
+		}
+		try {
+			return OBJECT_MAPPER.readValue(json, STRING_MAP_TYPE);
+		} catch ( Exception e ) {
+			LOG.error("Exception deserialzing JSON {} to Map<String, Object>", json, e);
+		}
+		return null;
+	}
+
+	/**
+	 * Convert a JSON tree object to a Map with string keys.
+	 * 
+	 * <p>
+	 * This is designed for simple values. An internal {@link ObjectMapper} will
+	 * be used, and all floating point values will be converted to
+	 * {@link BigDecimal} values to faithfully represent the data. All
+	 * exceptions while deserializing the object are caught and ignored.
+	 * </p>
+	 * 
+	 * @param node
+	 *        the JSON object to convert
+	 * @return the map, or {@literal null} if {@code node} is not a JSON object,
+	 *         is {@literal null}, or any exception occurs generating the JSON
+	 */
+	public static Map<String, Object> getStringMapFromTree(final JsonNode node) {
+		if ( node == null || !node.isObject() ) {
+			return null;
+		}
+		try {
+			return OBJECT_MAPPER.readValue(OBJECT_MAPPER.treeAsTokens(node), STRING_MAP_TYPE);
+		} catch ( Exception e ) {
+			LOG.error("Exception deserialzing JSON node {} to Map<String, Object>", node, e);
+		}
+		return null;
+	}
+
+	/**
+	 * Write metadata to a JSON generator.
+	 * 
+	 * @param generator
+	 *        The generator to write to.
+	 * @param meta
+	 *        The metadata to write.
+	 * @throws IOException
+	 *         if any IO error occurs
+	 */
+	public static void writeMetadata(JsonGenerator generator, GeneralDatumMetadata meta)
+			throws IOException {
+		if ( meta == null ) {
+			return;
+		}
+		Map<String, Object> m = meta.getM();
+		if ( m != null ) {
+			generator.writeObjectField("m", m);
+		}
+
+		Map<String, Map<String, Object>> pm = meta.getPm();
+		if ( pm != null ) {
+			generator.writeObjectField("pm", pm);
+		}
+
+		Set<String> t = meta.getT();
+		if ( t != null ) {
+			generator.writeArrayFieldStart("t");
+			for ( String tag : t ) {
+				generator.writeString(tag);
+			}
+			generator.writeEndArray();
+		}
+	}
 
 	/**
 	 * Parse a BigDecimal from a JSON object attribute value.
