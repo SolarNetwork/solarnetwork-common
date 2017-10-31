c0d556f08179772b4f9d8e42d0313a5e931e37d4
Matt Magoffin <matt@solarnetwork.net>
2017-10-31 17:28:46 +1300

    NET-108: port the Identifiable API from SolarNode into Common, to share with Central.

---
+++ net.solarnetwork.common/src/net/solarnetwork/domain/Identifiable.java
@@ -0,0 +1,65 @@
+/* ==================================================================
+ * Identifiable.java - 31/10/2017 6:30:03 AM
+ * 
+ * Copyright 2017 SolarNetwork.net Dev Team
+ * 
+ * This program is free software; you can redistribute it and/or 
+ * modify it under the terms of the GNU General Public License as 
+ * published by the Free Software Foundation; either version 2 of 
+ * the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License 
+ * along with this program; if not, write to the Free Software 
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 
+ * 02111-1307 USA
+ * ==================================================================
+ */
+
+package net.solarnetwork.domain;
+
+/**
+ * API for a standardized way of identifying services, to support configuring
+ * links to specific instances of a service at runtime. Many managed services in
+ * SolarNode allow any number of them to be deployed.
+ * 
+ * @author matt
+ * @version 1.0
+ */
+public interface Identifiable {
+
+	/**
+	 * Get a unique identifier for this service.
+	 * 
+	 * <p>
+	 * This should be meaningful to the service implementation, and should be
+	 * minimally unique between instances of the same service interface.
+	 * </p>
+	 * 
+	 * @return unique identifier (should never be {@literal null})
+	 */
+	String getUid();
+
+	/**
+	 * Get a grouping identifier for this service.
+	 * 
+	 * <p>
+	 * This should be meaningful to the service implementation.
+	 * </p>
+	 * 
+	 * @return a group identifier, or {@literal null} if not part of any group
+	 */
+	String getGroupUid();
+
+	/**
+	 * Get a friendly display name for this service.
+	 * 
+	 * @return a display name
+	 */
+	String getDisplayName();
+
+}
