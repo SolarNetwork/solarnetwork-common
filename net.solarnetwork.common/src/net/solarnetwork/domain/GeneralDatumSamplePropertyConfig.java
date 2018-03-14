674a8e7f6dab71966e99b3c007922b2f103675a0
Matt Magoffin <matt@solarnetwork.net>
2018-03-14 16:00:15 +1300

    Add helper class to join some sort of configuration with a sample property key + type. Will be useful in plugins that allow general configuration of sample properties for things like sample data sources.

---
+++ net.solarnetwork.common/src/net/solarnetwork/domain/GeneralDatumSamplePropertyConfig.java
@@ -0,0 +1,143 @@
+/* ==================================================================
+ * GeneralDatumSamplePropertyConfig.java - 14/03/2018 3:45:22 PM
+ * 
+ * Copyright 2018 SolarNetwork.net Dev Team
+ * 
+ * This program is free software; you can redistribute it and/or 
+ * modify it under the terms of the GNU General Public License as 
+ * published by the Free Software Foundation; either version 2 of 
+ * the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License 
+ * along with this program; if not, write to the Free Software 
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 
+ * 02111-1307 USA
+ * ==================================================================
+ */
+
+package net.solarnetwork.domain;
+
+/**
+ * Join some sort of configuration with a sample property key and type.
+ * 
+ * @param V
+ *        the configuration type
+ * @author matt
+ * @version 1.0
+ * @since 1.42
+ */
+public class GeneralDatumSamplePropertyConfig<V> {
+
+	private String propertyKey;
+	private GeneralDatumSamplesType propertyType;
+	private V config;
+
+	/**
+	 * Default constructor.
+	 */
+	public GeneralDatumSamplePropertyConfig() {
+		super();
+	}
+
+	/**
+	 * Construct with values.
+	 * 
+	 * @param propertyKey
+	 *        the sample property key to use
+	 * @param propertyType
+	 *        the sample property type to use
+	 * @param config
+	 *        the configuration to use
+	 */
+	public GeneralDatumSamplePropertyConfig(String propertyKey, GeneralDatumSamplesType propertyType,
+			V config) {
+		super();
+		setPropertyKey(propertyKey);
+		setPropertyType(propertyType);
+		setConfig(config);
+	}
+
+	/**
+	 * Get the sample property key.
+	 * 
+	 * <p>
+	 * This value represents a key in a
+	 * {@link GeneralDatumSamples#getSampleData(GeneralDatumSamplesType)} map.
+	 * </p>
+	 * 
+	 * @return the sample property key
+	 */
+	public String getPropertyKey() {
+		return propertyKey;
+	}
+
+	/**
+	 * Set the sample property key.
+	 * 
+	 * <p>
+	 * This value represents a key in a
+	 * {@link GeneralDatumSamples#getSampleData(GeneralDatumSamplesType)} map.
+	 * </p>
+	 * 
+	 * @param propertyKey
+	 *        the sample property key
+	 */
+	public void setPropertyKey(String propertyKey) {
+		this.propertyKey = propertyKey;
+	}
+
+	/**
+	 * Get the sample property type.
+	 * 
+	 * <p>
+	 * This value represents the type to use in a
+	 * {@link GeneralDatumSamples#getSampleData(GeneralDatumSamplesType)} map.
+	 * </p>
+	 * 
+	 * @return the sample property type
+	 */
+	public GeneralDatumSamplesType getPropertyType() {
+		return propertyType;
+	}
+
+	/**
+	 * Set the sample property type.
+	 * 
+	 * <p>
+	 * This value represents the type to use in a
+	 * {@link GeneralDatumSamples#getSampleData(GeneralDatumSamplesType)} map.
+	 * </p>
+	 * 
+	 * @param propertyType
+	 */
+	public void setPropertyType(GeneralDatumSamplesType propertyType) {
+		this.propertyType = propertyType;
+	}
+
+	/**
+	 * Get the configuration object.
+	 * 
+	 * @return the configuration to associate with the sample property key and
+	 *         type
+	 */
+	public V getConfig() {
+		return config;
+	}
+
+	/**
+	 * Set the configuration object.
+	 * 
+	 * @param config
+	 *        the configuration to associate with the sample property key and
+	 *        type
+	 */
+	public void setConfig(V config) {
+		this.config = config;
+	}
+
+}
