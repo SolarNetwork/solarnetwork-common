042d19267b26876d41c0e24c582a811c5365ca7c
Matt Magoffin <matt@solarnetwork.net>
2018-03-16 12:24:40 +1300

    Add a String node control property type.

--- net.solarnetwork.common/src/net/solarnetwork/domain/NodeControlPropertyType.java
+++ net.solarnetwork.common/src/net/solarnetwork/domain/NodeControlPropertyType.java
@@ -26,16 +26,57 @@ package net.solarnetwork.domain;
  * Enumeration of supported node component property types.
  * 
  * @author matt
- * @version 1.0
+ * @version 1.1
  */
 public enum NodeControlPropertyType {
 
-	Boolean,
+	/** A boolean on/off toggle control property. */
+	Boolean('b'),
 
-	Float,
+	/** A floating point decimal control property. */
+	Float('f'),
 
-	Integer,
+	/** A whole number control property. */
+	Integer('i'),
 
-	Percent;
+	/** A percentage control property. */
+	Percent('p'),
 
+	/** A string control property. */
+	String('s');
+
+	private final char key;
+
+	private NodeControlPropertyType(char key) {
+		this.key = key;
+	}
+
+	/**
+	 * Get a key value for this enum.
+	 * 
+	 * @return the key
+	 * @since 1.1
+	 */
+	public char getKey() {
+		return key;
+	}
+
+	/**
+	 * Get an enum instance for a key value.
+	 * 
+	 * @param key
+	 *        the key
+	 * @return the enum
+	 * @throws IllegalArgumentException
+	 *         if {@code key} is not a valid value
+	 * @since 1.1
+	 */
+	public static NodeControlPropertyType forKey(char key) {
+		for ( NodeControlPropertyType e : NodeControlPropertyType.values() ) {
+			if ( key == e.key ) {
+				return e;
+			}
+		}
+		throw new IllegalArgumentException("Unknown NodeControlPropertyType key [" + key + "]");
+	}
 }
