8f5d5a488bacad8d3ba0dd484fb8b197199a437e
Matt Magoffin <matt@solarnetwork.net>
2018-03-15 14:54:28 +1300

    Port DataUtils from SolarNode into common; add bigDecimalForNumber() common utility.

---
+++ net.solarnetwork.common/src/net/solarnetwork/util/NumberUtils.java
@@ -0,0 +1,105 @@
+/* ==================================================================
+ * NumberUtils.java - 15/03/2018 2:49:15 PM
+ * 
+ * Copyright 2018 SolarNetwork.net Dev Team
+ * 
+ * This program is free software; you can redistribute it and/or 
+ * modify it under the terms of the GNU General Public License as 
+ * published by the Free Software Foundation; either version 2 of 
+ * the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License 
+ * along with this program; if not, write to the Free Software 
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 
+ * 02111-1307 USA
+ * ==================================================================
+ */
+
+package net.solarnetwork.util;
+
+import java.math.BigDecimal;
+
+/**
+ * Utilities for dealing with numbers.
+ * 
+ * @author matt
+ * @version 1.0
+ * @since 1.42
+ */
+public final class NumberUtils {
+
+	private NumberUtils() {
+		// do not construct me
+	}
+
+	/**
+	 * Convert a signed byte into an unsigned short value.
+	 * 
+	 * <p>
+	 * The returned short will have a value between 0 and 255.
+	 * </p>
+	 * 
+	 * @param data
+	 *        the byte
+	 * @return the unsigned value
+	 */
+	public static short unsigned(byte data) {
+		return (short) (data & 0xFF);
+	}
+
+	/**
+	 * Convert signed bytes into unsigned short values.
+	 * 
+	 * <p>
+	 * The returned shorts will have values between 0 and 255.
+	 * </p>
+	 * 
+	 * @param data
+	 *        the bytes
+	 * @return the unsigned values
+	 */
+	public static short[] getUnsignedValues(byte[] data) {
+		// convert bytes into "unsigned" integer values, i.e. 0..255
+		short[] unsigned = new short[data.length];
+		for ( int i = 0; i < data.length; i++ ) {
+			unsigned[i] = (short) (data[i] & 0xFF);
+		}
+		return unsigned;
+	}
+
+	/**
+	 * Get a {@link BigDecimal} for a number.
+	 * 
+	 * <p>
+	 * If {@code value} is already a {@link BigDecimal} it will be returned
+	 * directly. Otherwise a new {@link BigDecimal} instance will be created out
+	 * of {@code value}.
+	 * </p>
+	 * 
+	 * @param value
+	 *        the number to get a {@code BigDecimal} version of
+	 * @return the {@code BigDecimal} version of {@code value}
+	 */
+	public static BigDecimal bigDecimalForNumber(Number value) {
+		BigDecimal v = null;
+		if ( value instanceof BigDecimal ) {
+			v = (BigDecimal) value;
+		} else if ( value instanceof Long ) {
+			v = new BigDecimal(value.longValue());
+		} else if ( value instanceof Integer || value instanceof Short ) {
+			v = new BigDecimal(value.intValue());
+		} else if ( value instanceof Double ) {
+			v = BigDecimal.valueOf(value.doubleValue());
+		} else {
+			// note Float falls through to here per recommended way of converting that to BigDecimal
+			v = new BigDecimal(value.toString());
+		}
+		return v;
+	}
+
+}
