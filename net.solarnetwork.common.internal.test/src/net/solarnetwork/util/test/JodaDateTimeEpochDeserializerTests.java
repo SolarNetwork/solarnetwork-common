d6668b71cb378bff2de7794a131b27022693b831
Matt Magoffin <matt@solarnetwork.net>
2019-11-06 09:41:10 +1300

    Add JSON de/serializers for millisecond epoch dates <-> DateTime.

---
+++ net.solarnetwork.common.internal.test/src/net/solarnetwork/util/test/JodaDateTimeEpochDeserializerTests.java
@@ -0,0 +1,91 @@
+/* ==================================================================
+ * JodaDateTimeEpochDeserializerTests.java - 6/11/2019 7:24:36 am
+ * 
+ * Copyright 2019 SolarNetwork.net Dev Team
+ * 
+ * This program is free software; you can redistribute it and/or 
+ * modify it under the terms of the GNU General Public License as 
+ * published by the Free Software Foundation; either version 2 of 
+ * the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License 
+ * along with this program; if not, write to the Free Software 
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 
+ * 02111-1307 USA
+ * ==================================================================
+ */
+
+package net.solarnetwork.util.test;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.nullValue;
+import static org.junit.Assert.assertThat;
+import org.joda.time.DateTime;
+import org.junit.Before;
+import org.junit.Test;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import net.solarnetwork.util.JodaDateTimeEpochDeserializer;
+
+/**
+ * Test cases for the {@link JodaDateTimeEpochDeserializer} class.
+ * 
+ * @author matt
+ * @version 1.0
+ */
+public class JodaDateTimeEpochDeserializerTests {
+
+	private ObjectMapper mapper;
+
+	private ObjectMapper createObjectMapper() {
+		ObjectMapper m = new ObjectMapper();
+		SimpleModule mod = new SimpleModule("Test");
+		mod.addDeserializer(DateTime.class, new JodaDateTimeEpochDeserializer());
+		m.registerModule(mod);
+		return m;
+	}
+
+	@Before
+	public void setup() {
+		mapper = createObjectMapper();
+	}
+
+	@Test
+	public void deserializeInteger() throws Exception {
+		long now = System.currentTimeMillis();
+		DateTime result = mapper.readValue(String.valueOf(now), DateTime.class);
+		assertThat("Parsed date", result, equalTo(new DateTime(now)));
+	}
+
+	@Test
+	public void deserializeFloat() throws Exception {
+		long now = System.currentTimeMillis();
+		DateTime result = mapper.readValue(String.valueOf(now) + ".0", DateTime.class);
+		assertThat("Parsed date", result, equalTo(new DateTime(now)));
+	}
+
+	@Test
+	public void deserializeString() throws Exception {
+		long now = System.currentTimeMillis();
+		DateTime result = mapper.readValue(String.format("\"%d\"", now), DateTime.class);
+		assertThat("Parsed date", result, equalTo(new DateTime(now)));
+	}
+
+	@Test(expected = JsonProcessingException.class)
+	public void deserializeEmptyString() throws Exception {
+		mapper.readValue("\"\"", DateTime.class);
+	}
+
+	@Test
+	public void deserializeNull() throws Exception {
+		DateTime result = mapper.readValue("null", DateTime.class);
+		assertThat("Parsed null to null", result, nullValue());
+	}
+
+}
---
+++ net.solarnetwork.common/src/net/solarnetwork/util/JodaDateTimeEpochDeserializer.java
@@ -0,0 +1,76 @@
+/* ==================================================================
+ * JodaDateTimeEpochDeserializer.java - 6/11/2019 7:15:02 am
+ * 
+ * Copyright 2019 SolarNetwork.net Dev Team
+ * 
+ * This program is free software; you can redistribute it and/or 
+ * modify it under the terms of the GNU General Public License as 
+ * published by the Free Software Foundation; either version 2 of 
+ * the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License 
+ * along with this program; if not, write to the Free Software 
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 
+ * 02111-1307 USA
+ * ==================================================================
+ */
+
+package net.solarnetwork.util;
+
+import java.io.IOException;
+import org.joda.time.DateTime;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.JsonToken;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.deser.std.StdScalarDeserializer;
+
+/**
+ * Deserialize millisecond epoch numbers into {@link DateTime} instances.
+ * 
+ * @author matt
+ * @version 1.0
+ * @since 1.55
+ */
+public class JodaDateTimeEpochDeserializer extends StdScalarDeserializer<DateTime> {
+
+	private static final long serialVersionUID = 7261772164817148373L;
+
+	/**
+	 * Constructor.
+	 */
+	public JodaDateTimeEpochDeserializer() {
+		super(DateTime.class);
+	}
+
+	@Override
+	public DateTime deserialize(JsonParser p, DeserializationContext ctxt)
+			throws IOException, JsonProcessingException {
+		JsonToken t = p.currentToken();
+		long l = 0;
+		if ( t == JsonToken.VALUE_NULL ) {
+			return null;
+		} else if ( t == JsonToken.VALUE_NUMBER_INT || t == JsonToken.VALUE_NUMBER_FLOAT ) {
+			l = p.getLongValue();
+		} else {
+			String s = p.getValueAsString();
+			if ( s == null ) {
+				return null;
+			}
+			try {
+				l = Long.parseLong(s);
+			} catch ( NumberFormatException e ) {
+				throw new JsonParseException(p,
+						"Unable to parse millisecond epoch timestamp from [" + s + "]", e);
+			}
+		}
+		return new DateTime(l);
+	}
+
+}
