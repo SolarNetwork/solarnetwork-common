de71e2b71d958ef3b7802e921ed4de404530d75f
Matt Magoffin <matt@solarnetwork.net>
2021-05-13 07:20:19 +1200

    Flush out CSV parsing test.

--- net.solarnetwork.common.internal.test/src/net/solarnetwork/domain/tariff/test/CsvTemporalRangeTariffParserTests.java
+++ net.solarnetwork.common.internal.test/src/net/solarnetwork/domain/tariff/test/CsvTemporalRangeTariffParserTests.java
@@ -22,13 +22,21 @@
 
 package net.solarnetwork.domain.tariff.test;
 
+import static java.util.stream.Collectors.toList;
+import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.notNullValue;
 import static org.junit.Assert.assertThat;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.math.BigDecimal;
 import java.util.List;
+import java.util.Locale;
+import java.util.stream.IntStream;
 import org.junit.Test;
 import net.solarnetwork.domain.tariff.CsvTemporalRangeTariffParser;
+import net.solarnetwork.domain.tariff.SimpleTariffRate;
+import net.solarnetwork.domain.tariff.Tariff.Rate;
 import net.solarnetwork.domain.tariff.TemporalRangesTariff;
 
 /**
@@ -38,6 +46,54 @@ import net.solarnetwork.domain.tariff.TemporalRangesTariff;
  * @version 1.0
  */
 public class CsvTemporalRangeTariffParserTests {
+
+	/**
+	 * Create a tariff where the rates names are given "column" names like in a
+	 * spreadsheet, starting with "E".
+	 * 
+	 * @param locale
+	 *        the locale, or {@literal null} to use the system default
+	 * @param data
+	 *        the month, day, day of week, time, rate1[, rateN...]
+	 * @return the tariff
+	 */
+	public static TemporalRangesTariff spreadsheetColumnRates(Locale locale, String... data) {
+		locale = (locale != null ? locale : Locale.getDefault());
+		List<Rate> rates = IntStream.range(4, data.length).mapToObj(
+				i -> new SimpleTariffRate(Character.toString((char) ('A' + i)), new BigDecimal(data[i])))
+				.collect(toList());
+		return new TemporalRangesTariff(data[0], data[1], data[2], data[3], rates, locale);
+	}
+
+	/**
+	 * Create a tariff where the rates names are given "column" names like in a
+	 * spreadsheet, starting with "E".
+	 * 
+	 * @param data
+	 *        the month, day, day of week, time, rate1[, rateN...]
+	 * @return the tariff
+	 */
+	public static TemporalRangesTariff spreadsheetColumnRates(String... data) {
+		return spreadsheetColumnRates(null, data);
+	}
+
+	public static void assertTemporalRangeTariff(String msg, TemporalRangesTariff actual,
+			TemporalRangesTariff expected) {
+		assertThat(msg + " tariff exists", actual, notNullValue());
+		assertThat(msg + " month range", actual.getMonthRange(), equalTo(expected.getMonthRange()));
+		assertThat(msg + " day of month range", actual.getDayOfMonthRange(),
+				equalTo(expected.getDayOfMonthRange()));
+		assertThat(msg + " day of week range", actual.getDayOfWeekRange(),
+				equalTo(expected.getDayOfWeekRange()));
+		assertThat(msg + " minute of day range", actual.getMinuteOfDayRange(),
+				equalTo(expected.getMinuteOfDayRange()));
+		assertThat(msg + " has no rates", actual.getRates(), equalTo(expected.getRates()));
+	}
+
+	public static void assertTemporalRangeTariff(String msg, TemporalRangesTariff actual,
+			String... data) {
+		assertTemporalRangeTariff(msg, actual, spreadsheetColumnRates(data));
+	}
 
 	@Test
 	public void parse_example() throws IOException {
@@ -50,7 +106,14 @@ public class CsvTemporalRangeTariffParserTests {
 			List<TemporalRangesTariff> tariffs = p.parseTariffs(r);
 
 			assertThat("Tariffs parsed", tariffs, hasSize(4));
-			// TODO more checks
+			assertTemporalRangeTariff("Row 1", tariffs.get(0), "January-December", null, "Mon-Fri",
+					"0-8", "10.48");
+			assertTemporalRangeTariff("Row 2", tariffs.get(1), "January-December", null, "Mon-Fri",
+					"8-24", "11.00");
+			assertTemporalRangeTariff("Row 3", tariffs.get(2), "January-December", null, "Sat-Sun",
+					"0-8", "9.19");
+			assertTemporalRangeTariff("Row 4", tariffs.get(3), "January-December", null, "Sat-Sun",
+					"8-24", "11.21");
 		}
 	}
 
