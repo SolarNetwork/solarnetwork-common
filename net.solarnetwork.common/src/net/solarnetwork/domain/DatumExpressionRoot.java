757b9e4fc5f94367b7bca98e2c2256c89160f3b5
Matt Magoffin <matt@solarnetwork.net>
2021-05-14 11:48:10 +1200

    Add a common expression root that is a Map implementation to make writing expressions very easy.

---
+++ net.solarnetwork.common/src/net/solarnetwork/domain/DatumExpressionRoot.java
@@ -0,0 +1,58 @@
+/* ==================================================================
+ * DatumExpressionRoot.java - 9/05/2021 11:18:13 AM
+ * 
+ * Copyright 2021 SolarNetwork.net Dev Team
+ * 
+ * This program is free software; you can redistribute it and/or 
+ * modify it under the terms of the GNU General Public License as 
+ * published by the Free Software Foundation; either version 2 of 
+ * the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License 
+ * along with this program; if not, write to the Free Software 
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 
+ * 02111-1307 USA
+ * ==================================================================
+ */
+
+package net.solarnetwork.domain;
+
+import java.util.Map;
+import net.solarnetwork.domain.datum.Datum;
+
+/**
+ * API for a datum-based expression root.
+ * 
+ * @author matt
+ * @version 1.0
+ * @since 1.71
+ */
+public interface DatumExpressionRoot {
+
+	/**
+	 * Get the datum.
+	 * 
+	 * @return the datum
+	 */
+	Datum getDatum();
+
+	/**
+	 * Get the datum properties, i.e. {@link Datum#getSampleData()}.
+	 * 
+	 * @return the datum properties
+	 */
+	Map<String, ?> getProps();
+
+	/**
+	 * Get additional data.
+	 * 
+	 * @return the data
+	 */
+	Map<String, ?> getData();
+
+}
