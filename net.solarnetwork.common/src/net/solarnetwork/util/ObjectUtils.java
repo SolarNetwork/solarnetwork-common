60135fd3d08748479340bec534b4712c1ea8ddb6
Matt Magoffin <matt@solarnetwork.net>
2022-05-01 08:05:52 +1200

    Add helpers for non-empty arguments.

--- net.solarnetwork.common/src/net/solarnetwork/util/ObjectUtils.java
+++ net.solarnetwork.common/src/net/solarnetwork/util/ObjectUtils.java
@@ -22,11 +22,13 @@
 
 package net.solarnetwork.util;
 
+import java.util.Collection;
+
 /**
  * Utilities for dealing with objects.
  * 
  * @author matt
- * @version 1.0
+ * @version 1.1
  */
 public final class ObjectUtils {
 
@@ -65,6 +67,82 @@ public final class ObjectUtils {
 		if ( arg == null ) {
 			throw new IllegalArgumentException(
 					String.format("The %s argument must not be null.", argumentName));
+		}
+		return arg;
+	}
+
+	/**
+	 * Require a non-empty array method argument.
+	 * 
+	 * <p>
+	 * This is similar to
+	 * {@link java.util.Objects#requireNonNull(Object, String)} except
+	 * {@code argumentName} is just the name of the required argument and an
+	 * {@link IllegalArgumentException} is thrown instead of a
+	 * {@code NullPointerException}. Example use:
+	 * </p>
+	 * 
+	 * <!-- @formatter:off -->
+	 * <blockquote><pre>
+	 * public Foo(Bar[] bar) {
+	 * 	this.bar = ObjectUtils.requireNonNullArgument(bar, "bar");
+	 * }
+	 * </pre></blockquote>
+	 * <!-- @formatter:on -->
+	 * 
+	 * @param <T>
+	 *        the argument type
+	 * @param arg
+	 *        the argument to require to be non-empty
+	 * @param argumentName
+	 *        the name of {@code arg} to report in the
+	 *        {@link IllegalArgumentException} if {@code arg} is {@literal null}
+	 * @return {@code arg}
+	 * @throws IllegalArgumentException
+	 *         if {@code arg} is {@literal null} or empty
+	 */
+	public static <T> T[] requireNonEmptyArgument(T[] arg, String argumentName) {
+		if ( arg == null || arg.length < 1 ) {
+			throw new IllegalArgumentException(
+					String.format("The %s argument must not be empty.", argumentName));
+		}
+		return arg;
+	}
+
+	/**
+	 * Require a non-empty array method argument.
+	 * 
+	 * <p>
+	 * This is similar to
+	 * {@link java.util.Objects#requireNonNull(Object, String)} except
+	 * {@code argumentName} is just the name of the required argument and an
+	 * {@link IllegalArgumentException} is thrown instead of a
+	 * {@code NullPointerException}. Example use:
+	 * </p>
+	 * 
+	 * <!-- @formatter:off -->
+	 * <blockquote><pre>
+	 * public Foo(List&lt;Bar&gt; bar) {
+	 * 	this.bar = ObjectUtils.requireNonNullArgument(bar, "bar");
+	 * }
+	 * </pre></blockquote>
+	 * <!-- @formatter:on -->
+	 * 
+	 * @param <T>
+	 *        the argument type
+	 * @param arg
+	 *        the argument to require to be non-empty
+	 * @param argumentName
+	 *        the name of {@code arg} to report in the
+	 *        {@link IllegalArgumentException} if {@code arg} is {@literal null}
+	 * @return {@code arg}
+	 * @throws IllegalArgumentException
+	 *         if {@code arg} is {@literal null} or empty
+	 */
+	public static <T extends Collection<?>> T requireNonEmptyArgument(T arg, String argumentName) {
+		if ( arg == null || arg.size() < 1 ) {
+			throw new IllegalArgumentException(
+					String.format("The %s argument must not be empty.", argumentName));
 		}
 		return arg;
 	}
