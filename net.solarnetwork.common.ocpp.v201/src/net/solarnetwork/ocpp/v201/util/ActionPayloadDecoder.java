1c36c6f264996791f3ec47e6deefa0329f3d914d
Matt Magoffin <matt@solarnetwork.net>
2024-02-19 08:17:59 +1300

    Add more OCPP 2 enumeration values.

--- net.solarnetwork.common.ocpp.v201/src/net/solarnetwork/ocpp/v201/util/ActionPayloadDecoder.java
+++ net.solarnetwork.common.ocpp.v201/src/net/solarnetwork/ocpp/v201/util/ActionPayloadDecoder.java
@@ -1,21 +1,21 @@
 /* ==================================================================
  * ActionPayloadDecoder.java - 12/02/2024 5:45:00 pm
- * 
+ *
  * Copyright 2024 SolarNetwork.net Dev Team
- * 
- * This program is free software; you can redistribute it and/or 
- * modify it under the terms of the GNU General Public License as 
- * published by the Free Software Foundation; either version 2 of 
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
  * the License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful, 
- * but WITHOUT ANY WARRANTY; without even the implied warranty of 
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License 
- * along with this program; if not, write to the Free Software 
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
  * 02111-1307 USA
  * ==================================================================
  */
@@ -25,21 +25,24 @@ package net.solarnetwork.ocpp.v201.util;
 import static net.solarnetwork.ocpp.v201.util.OcppUtils.parseOcppMessage;
 import java.io.IOException;
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.networknt.schema.JsonSchemaFactory;
 import net.solarnetwork.ocpp.domain.Action;
 import net.solarnetwork.ocpp.domain.SchemaValidationException;
+import net.solarnetwork.util.ObjectUtils;
 
 /**
  * Implementation of {@link net.solarnetwork.ocpp.json.ActionPayloadDecoder}
  * with schema validation support.
- * 
+ *
  * @author matt
- * @version 1.0
+ * @version 1.1
  */
 public class ActionPayloadDecoder implements net.solarnetwork.ocpp.json.ActionPayloadDecoder {
 
 	private final JsonSchemaFactory validator;
+	private final ObjectMapper objectMapper;
 
 	/**
 	 * Constructor.
@@ -50,12 +53,28 @@ public class ActionPayloadDecoder implements net.solarnetwork.ocpp.json.ActionPa
 
 	/**
 	 * Constructor.
-	 * 
+	 *
 	 * @param validator
 	 *        the optional validator to use
 	 */
 	public ActionPayloadDecoder(JsonSchemaFactory validator) {
+		this(OcppUtils.newObjectMapper(), validator);
+	}
+
+	/**
+	 * Constructor.
+	 *
+	 * @param validator
+	 *        the optional validator to use
+	 * @param objectMapper
+	 *        the mapper to use
+	 * @throws IllegalArgumentException
+	 *         if {@code mapper} is {@literal null}
+	 * @since 1.1
+	 */
+	public ActionPayloadDecoder(ObjectMapper objectMapper, JsonSchemaFactory validator) {
 		super();
+		this.objectMapper = ObjectUtils.requireNonNullArgument(objectMapper, "objectMapper");
 		this.validator = validator;
 	}
 
@@ -69,7 +88,8 @@ public class ActionPayloadDecoder implements net.solarnetwork.ocpp.json.ActionPa
 			throw new SchemaValidationException(payload, "Message is not a JSON object.");
 		}
 		@SuppressWarnings("unchecked")
-		T result = (T) parseOcppMessage(action.getName(), !forResult, (ObjectNode) payload, validator);
+		T result = (T) parseOcppMessage(action.getName(), !forResult, (ObjectNode) payload, validator,
+				objectMapper);
 		return result;
 	}
 
