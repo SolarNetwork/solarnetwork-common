09a48f1e4ff2381bc2e5d28ebd61cd970b1de49d
Matt Magoffin <matt@solarnetwork.net>
2024-06-13 12:42:38 +1200

    Allow HikariCP up to version 6 after testing with 5.1.

--- net.solarnetwork.common.jdbc.pool.hikari/src/net/solarnetwork/common/jdbc/pool/hikari/Activator.java
+++ net.solarnetwork.common.jdbc.pool.hikari/src/net/solarnetwork/common/jdbc/pool/hikari/Activator.java
@@ -1,21 +1,21 @@
 /* ==================================================================
  * Activator.java - 26/07/2019 11:38:17 am
- * 
+ *
  * Copyright 2019 SolarNetwork.net Dev Team
- * 
- * This program is free software; you can redistribute it and/or 
- * modify it under the terms of the GNU General Public License as 
- * published by the Free Software Foundation; either version 2 of 
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
  * the License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful, 
- * but WITHOUT ANY WARRANTY; without even the implied warranty of 
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License 
- * along with this program; if not, write to the Free Software 
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
  * 02111-1307 USA
  * ==================================================================
  */
@@ -31,7 +31,7 @@ import org.osgi.service.cm.ManagedServiceFactory;
 
 /**
  * Bundle activator for the connection pool.
- * 
+ *
  * @author matt
  * @version 1.0
  */
@@ -44,6 +44,13 @@ public class Activator implements BundleActivator {
 	public static final String SERVICE_PID = "net.solarnetwork.jdbc.pool.hikari";
 
 	private ServiceRegistration<ManagedServiceFactory> msf = null;
+
+	/**
+	 * Constructor.
+	 */
+	public Activator() {
+		super();
+	}
 
 	@Override
 	public void start(BundleContext context) throws Exception {
