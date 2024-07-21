857e1bd2b9f7787f89d913318d92c7b4bbb8451b
Matt Magoffin <matt@solarnetwork.net>
2024-07-22 07:21:13 +1200

    Add work around for OCPP-J 1.6 errata 2.1 (typo "Celcius").

--- net.solarnetwork.common.ocpp.v16.test/src/net/solarnetwork/ocpp/v16/cs/test/MeterValuesProcessorTests.java
+++ net.solarnetwork.common.ocpp.v16.test/src/net/solarnetwork/ocpp/v16/cs/test/MeterValuesProcessorTests.java
@@ -1,21 +1,21 @@
 /* ==================================================================
  * MeterValuesProcessorTests.java - 15/02/2020 9:15:12 am
- * 
+ *
  * Copyright 2020 SolarNetwork.net Dev Team
- * 
- * This program is free software; you can redistribute it and/or 
- * modify it under the terms of the GNU General Public License as 
- * published by the Free Software Foundation; either version 2 of 
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
  * the License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful, 
- * but WITHOUT ANY WARRANTY; without even the implied warranty of 
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License 
- * along with this program; if not, write to the Free Software 
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
  * 02111-1307 USA
  * ==================================================================
  */
@@ -65,9 +65,9 @@ import ocpp.v16.cs.UnitOfMeasure;
 
 /**
  * Test cases for the {@link MeterValuesProcessor} class.
- * 
+ *
  * @author matt
- * @version 1.2
+ * @version 1.3
  */
 public class MeterValuesProcessorTests {
 
@@ -177,6 +177,78 @@ public class MeterValuesProcessorTests {
 				assertThat("Value copied 1", sve.getValue(), equalTo("3000"));
 			}
 		}
+	}
+
+	@SuppressWarnings("deprecation")
+	@Test
+	public void process_celcius() throws InterruptedException {
+		// GIVEN
+		CountDownLatch l = new CountDownLatch(1);
+		ChargePointIdentity clientId = createClientId();
+		ChargePoint cp = new ChargePoint(UUID.randomUUID().getMostSignificantBits(), Instant.now(),
+				new ChargePointInfo(clientId.getIdentifier()));
+		String idTag = UUID.randomUUID().toString().substring(0, 20);
+		int transactionId = 1;
+		String txId = String.valueOf(transactionId);
+
+		ChargeSession session = new ChargeSession(UUID.randomUUID(), Instant.now(), idTag, cp.getId(), 1,
+				txId);
+		expect(chargeSessionManager.getActiveChargingSession(clientId, txId)).andReturn(session);
+
+		Capture<Iterable<net.solarnetwork.ocpp.domain.SampledValue>> readingsCaptor = Capture
+				.newInstance();
+		chargeSessionManager.addChargingSessionReadings(eq(clientId), isNull(),
+				eq(session.getConnectorId()), capture(readingsCaptor));
+
+		// WHEN
+		replayAll();
+		MeterValuesRequest req = new MeterValuesRequest();
+		req.setConnectorId(session.getConnectorId());
+		req.setTransactionId(transactionId);
+
+		MeterValue mv = new MeterValue();
+		mv.setTimestamp(XmlDateUtils.newXmlCalendar(2020, 02, 14, 10, 0, 0, 0));
+		SampledValue sv = new SampledValue();
+		sv.setContext(ReadingContext.SAMPLE_PERIODIC);
+		sv.setLocation(Location.OUTLET);
+		sv.setMeasurand(Measurand.TEMPERATURE);
+		sv.setUnit(UnitOfMeasure.CELCIUS);
+		sv.setValue("1234");
+		mv.getSampledValue().add(sv);
+		req.getMeterValue().add(mv);
+
+		ActionMessage<MeterValuesRequest> message = new BasicActionMessage<MeterValuesRequest>(clientId,
+				CentralSystemAction.MeterValues, req);
+		processor.processActionMessage(message, (msg, res, err) -> {
+			assertThat("Message passed", msg, sameInstance(message));
+			assertThat("Result available", res, notNullValue());
+			assertThat("No error", err, nullValue());
+
+			l.countDown();
+			return true;
+		});
+
+		// THEN
+		assertThat("Result handler invoked", l.await(1, TimeUnit.SECONDS), equalTo(true));
+
+		List<net.solarnetwork.ocpp.domain.SampledValue> txData = StreamSupport
+				.stream(readingsCaptor.getValue().spliterator(), false).collect(Collectors.toList());
+		assertThat("1 sampled value entity created", txData, hasSize(1));
+
+		net.solarnetwork.ocpp.domain.SampledValue sve = txData.get(0);
+		assertThat("Session ID populated", sve.getSessionId(), equalTo(session.getId()));
+		assertThat("Timestatmp", sve.getTimestamp(),
+				equalTo(XmlDateUtils.timestamp(mv.getTimestamp(), null)));
+		assertThat("Reading context translated", sve.getContext(),
+				equalTo(net.solarnetwork.ocpp.domain.ReadingContext.SamplePeriodic));
+		assertThat("Location translated", sve.getLocation(),
+				equalTo(net.solarnetwork.ocpp.domain.Location.Outlet));
+
+		assertThat("Measurand translated 0", sve.getMeasurand(),
+				equalTo(net.solarnetwork.ocpp.domain.Measurand.Temperature));
+		assertThat("Unit trarnslated 0 (correct Celcius to Celsius)", sve.getUnit(),
+				equalTo(net.solarnetwork.ocpp.domain.UnitOfMeasure.Celsius));
+		assertThat("Value copied 0", sve.getValue(), equalTo("1234"));
 	}
 
 	@Test
