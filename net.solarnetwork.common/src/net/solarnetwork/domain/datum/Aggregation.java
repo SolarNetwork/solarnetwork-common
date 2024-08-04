b3af7431981d31bf2d07b271813f0906f10032f1
Matt Magoffin <matt@solarnetwork.net>
2024-08-05 09:08:11 +1200

    NET-385, NET-386: add DayOfYear and HourOfYear aggregate levels.

--- net.solarnetwork.common/src/net/solarnetwork/domain/datum/Aggregation.java
+++ net.solarnetwork.common/src/net/solarnetwork/domain/datum/Aggregation.java
@@ -1,23 +1,23 @@
 /* ===================================================================
  * Aggregation.java
- * 
+ *
  * Created Dec 1, 2009 4:10:14 PM
- * 
+ *
  * Copyright 2007 SolarNetwork.net Dev Team
- * 
- * This program is free software; you can redistribute it and/or 
- * modify it under the terms of the GNU General Public License as 
- * published by the Free Software Foundation; either version 2 of 
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
  * the License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful, 
- * but WITHOUT ANY WARRANTY; without even the implied warranty of 
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License 
- * along with this program; if not, write to the Free Software 
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
  * 02111-1307 USA
  * ===================================================================
  */
@@ -26,17 +26,13 @@ package net.solarnetwork.domain.datum;
 
 /**
  * An aggregation level enumeration.
- * 
- * @version 1.0
+ *
+ * @version 1.1
  * @since 2.7
  */
 public enum Aggregation {
 
-	/**
-	 * No aggregation.
-	 * 
-	 * @since 1.6
-	 */
+	/** No aggregation. */
 	None(0, "0"),
 
 	/** Minute level aggregation. */
@@ -51,11 +47,7 @@ public enum Aggregation {
 	/** Fifteen minute level aggregation. */
 	FifteenMinute(60 * 15, "15m"),
 
-	/**
-	 * Thirty minute level aggregation.
-	 * 
-	 * @since 1.5
-	 */
+	/** Thirty minute level aggregation. */
 	ThirtyMinute(60 * 30, "30m"),
 
 	/** Hour level aggregation. */
@@ -65,6 +57,11 @@ public enum Aggregation {
 	 * Aggregate by hour of the day, e.g. compare 12-1pm across multiple days.
 	 */
 	HourOfDay(3600, "hd"),
+
+	/**
+	 * Aggregate by hour of the day, e.g. compare 12-1pm across multiple days.
+	 */
+	HourOfYear(3600, "hy"),
 
 	/** Aggregate by hour of the day per season. */
 	SeasonalHourOfDay(3600, "shd"),
@@ -77,6 +74,14 @@ public enum Aggregation {
 	 * across multiple weeks.
 	 */
 	DayOfWeek(86400, "wd"),
+
+	/**
+	 * Aggregate by day of the year, e.g. compare up to 366 calendar days across
+	 * years.
+	 *
+	 * @since 1.1
+	 */
+	DayOfYear(86400, "yd"),
 
 	/** Aggregate by day of the week per season. */
 	SeasonalDayOfWeek(86400, "swd"),
@@ -93,18 +98,10 @@ public enum Aggregation {
 	/** Month level aggregation. */
 	Month(2419200, "M"),
 
-	/**
-	 * Year level aggregation.
-	 * 
-	 * @since 1.6
-	 */
+	/** Year level aggregation. */
 	Year(31536000, "y"),
 
-	/**
-	 * Aggregate all values into a single total result.
-	 * 
-	 * @since 1.4
-	 */
+	/** Aggregate all values into a single total result. */
 	RunningTotal(Integer.MAX_VALUE, "rt");
 
 	private final Integer level;
@@ -117,7 +114,7 @@ public enum Aggregation {
 
 	/**
 	 * Compare the level of this to another.
-	 * 
+	 *
 	 * @param other
 	 *        the other
 	 * @return -1 if this level less than other level, 0 if levels are equal, or
@@ -129,18 +126,18 @@ public enum Aggregation {
 
 	/**
 	 * Get the number of seconds the aggregation level represents.
-	 * 
+	 *
 	 * <p>
 	 * For aggregation levels higher than {@link #Day} the number of seconds are
 	 * approximate, based on the following standardized periods:
 	 * </p>
-	 * 
+	 *
 	 * <ul>
 	 * <li><b>Week</b> - 7 days</li>
 	 * <li><b>Month</b> - 4 weeks (28 days)</li>
 	 * <li><b>Year</b> - 365 days</li>
 	 * </ul>
-	 * 
+	 *
 	 * @return the aggregation level
 	 */
 	public Integer getLevel() {
@@ -149,9 +146,8 @@ public enum Aggregation {
 
 	/**
 	 * Get a key value.
-	 * 
+	 *
 	 * @return the key
-	 * @since 1.6
 	 */
 	public String getKey() {
 		return key;
@@ -159,14 +155,13 @@ public enum Aggregation {
 
 	/**
 	 * Get an enum instance for a key value.
-	 * 
+	 *
 	 * @param key
 	 *        the key value; if {@literal null} or empty then {@link #None} will
 	 *        be returned
 	 * @return the enum
 	 * @throws IllegalArgumentException
 	 *         if {@code key} is not supported
-	 * @since 1.6
 	 */
 	public static Aggregation forKey(String key) {
 		if ( key == null || key.isEmpty() ) {
