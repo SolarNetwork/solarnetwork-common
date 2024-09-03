fc5e4e87a9894171329495c610ad82fbc9265013
Matt Magoffin <matt@solarnetwork.net>
2024-09-03 18:19:14 +1200

    Fix signing date formatting bug if run in NZ locale, where Sept generated instead of Sep.

--- net.solarnetwork.common.web.test/src/net/solarnetwork/web/security/test/SecurityWebTestUtils.java
+++ net.solarnetwork.common.web.test/src/net/solarnetwork/web/security/test/SecurityWebTestUtils.java
@@ -1,21 +1,21 @@
 /* ==================================================================
  * SecurityWebTestUtils.java - 2/03/2017 12:37:16 PM
- * 
+ *
  * Copyright 2007-2017 SolarNetwork.net Dev Team
- * 
- * This program is free software; you can redistribute it and/or 
- * modify it under the terms of the GNU General Public License as 
- * published by the Free Software Foundation; either version 2 of 
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
  * the License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful, 
- * but WITHOUT ANY WARRANTY; without even the implied warranty of 
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License 
- * along with this program; if not, write to the Free Software 
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
  * 02111-1307 USA
  * ==================================================================
  */
@@ -25,15 +25,14 @@ package net.solarnetwork.web.security.test;
 import java.io.UnsupportedEncodingException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
-import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.TimeZone;
 import javax.crypto.Mac;
 import javax.crypto.spec.SecretKeySpec;
+import net.solarnetwork.security.AuthorizationUtils;
 
 /**
  * Utilities for unit tests.
- * 
+ *
  * @author matt
  * @version 1.0
  */
@@ -78,15 +77,11 @@ public final class SecurityWebTestUtils {
 	}
 
 	public static String httpDate(Date date) {
-		SimpleDateFormat sdf = new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss zzz");
-		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
-		return sdf.format(date);
+		return AuthorizationUtils.AUTHORIZATION_DATE_HEADER_FORMATTER.format(date.toInstant());
 	}
 
 	public static String iso8601Date(Date date) {
-		SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd'T'HHmmss'Z'");
-		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
-		return sdf.format(date);
+		return AuthorizationUtils.AUTHORIZATION_TIMESTAMP_FORMATTER.format(date.toInstant());
 	}
 
 }
