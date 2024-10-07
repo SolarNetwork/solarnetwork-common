cef4a4e9f6dc1aaa8fc0b4ab77d0bcd6b927cce5
Matt Magoffin <matt@solarnetwork.net>
2024-10-08 11:04:19 +1300

    Prevent object construction when no root object provided.

--- net.solarnetwork.common.expr.spel/src/net/solarnetwork/common/expr/spel/RestrictedEvaluationContext.java
+++ net.solarnetwork.common.expr.spel/src/net/solarnetwork/common/expr/spel/RestrictedEvaluationContext.java
@@ -1,21 +1,21 @@
 /* ==================================================================
  * RestrictedEvaluationContext.java - 6/02/2019 7:15:48 am
- * 
+ *
  * Copyright 2019 SolarNetwork.net Dev Team
- * 
- * This program is free software; you can redistribute it and/or 
- * modify it under the terms of the GNU General Public License as 
- * published by the Free Software Foundation; either version 2 of 
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
  * the License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful, 
- * but WITHOUT ANY WARRANTY; without even the implied warranty of 
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License 
- * along with this program; if not, write to the Free Software 
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
  * 02111-1307 USA
  * ==================================================================
  */
@@ -28,9 +28,9 @@ import org.springframework.expression.spel.support.StandardEvaluationContext;
 /**
  * {@link org.springframework.expression.EvaluationContext} that restricts some
  * runtime features.
- * 
+ *
  * @author matt
- * @version 1.0
+ * @version 1.1
  */
 public class RestrictedEvaluationContext extends StandardEvaluationContext {
 
@@ -39,11 +39,14 @@ public class RestrictedEvaluationContext extends StandardEvaluationContext {
 	 */
 	public RestrictedEvaluationContext() {
 		super();
+
+		// prevent construction of objects
+		setConstructorResolvers(emptyList());
 	}
 
 	/**
 	 * Constructor.
-	 * 
+	 *
 	 * @param rootObject
 	 *        the optional root object
 	 */
