3b9bd3bf2a08652f0374c809ccb28789de3d5180
Matt Magoffin <matt@solarnetwork.net>
2024-10-08 11:46:56 +1300

    Fix import.

--- net.solarnetwork.common.expr.spel.test/src/net/solarnetwork/common/expr/spel/test/SpelExpressionServiceTests.java
+++ net.solarnetwork.common.expr.spel.test/src/net/solarnetwork/common/expr/spel/test/SpelExpressionServiceTests.java
@@ -22,10 +22,10 @@
 
 package net.solarnetwork.common.expr.spel.test;
 
-import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.arrayContaining;
 import static org.hamcrest.Matchers.contains;
-import static org.junit.Assert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 import static org.junit.Assert.fail;
 import java.math.BigDecimal;
 import java.math.BigInteger;
