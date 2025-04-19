b8f4211e97d9137246f6bc8ceefa5b0ee4de7a41
Matt Magoffin <matt@solarnetwork.net>
2025-04-19 17:35:33 +1200

    Include "id" when serializing Location if it is also an Identity.
    Signed-off-by: Matt Magoffin <matt@solarnetwork.net>

--- net.solarnetwork.common/src/net/solarnetwork/codec/BasicLocationSerializer.java
+++ net.solarnetwork.common/src/net/solarnetwork/codec/BasicLocationSerializer.java
@@ -1,21 +1,21 @@
 /* ==================================================================
  * BasicLocationSerializer.java - 6/06/2021 9:56:10 AM
- * 
+ *
  * Copyright 2021 SolarNetwork.net Dev Team
- * 
- * This program is free software; you can redistribute it and/or 
- * modify it under the terms of the GNU General Public License as 
- * published by the Free Software Foundation; either version 2 of 
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
  * the License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful, 
- * but WITHOUT ANY WARRANTY; without even the implied warranty of 
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License 
- * along with this program; if not, write to the Free Software 
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
  * 02111-1307 USA
  * ==================================================================
  */
@@ -27,13 +27,14 @@ import java.io.Serializable;
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.SerializerProvider;
 import com.fasterxml.jackson.databind.ser.std.StdScalarSerializer;
+import net.solarnetwork.domain.Identity;
 import net.solarnetwork.domain.Location;
 
 /**
  * Serializer for {@link Location} objects.
- * 
+ *
  * @author matt
- * @version 1.1
+ * @version 1.2
  * @since 1.72
  */
 public class BasicLocationSerializer extends StdScalarSerializer<Location> implements Serializable {
@@ -57,8 +58,13 @@ public class BasicLocationSerializer extends StdScalarSerializer<Location> imple
 			generator.writeNull();
 			return;
 		}
+
+		Object id = (value instanceof Identity<?> ? ((Identity<?>) value).getId() : null);
+
 		// @formatter:off
-		final int size = (value.getName() != null ? 1 : 0)
+		final int size =
+				  (id != null ? 1 : 0)
+				+ (value.getName() != null ? 1 : 0)
 				+ (value.getCountry() != null ? 1 : 0)
 				+ (value.getRegion() != null ? 1 : 0)
 				+ (value.getStateOrProvince() != null ? 1 : 0)
@@ -72,6 +78,14 @@ public class BasicLocationSerializer extends StdScalarSerializer<Location> imple
 				;
 		// @formatter:on
 		generator.writeStartObject(value, size);
+		if ( id != null ) {
+			generator.writeFieldName("id");
+			if ( id instanceof Long ) {
+				generator.writeNumber((Long) id);
+			} else {
+				generator.writeString(id.toString());
+			}
+		}
 		BasicLocationField.Name.writeValue(generator, provider, value.getName());
 		BasicLocationField.Country.writeValue(generator, provider, value.getCountry());
 		BasicLocationField.Region.writeValue(generator, provider, value.getRegion());
