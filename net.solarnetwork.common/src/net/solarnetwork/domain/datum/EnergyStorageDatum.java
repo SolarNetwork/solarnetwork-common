5d918469c681c1d243ab801a01c7c43149a933b5
Matt Magoffin <matt@solarnetwork.net>
2025-05-27 09:49:54 +1200

    Remove @Deprecated code no longer needed.

--- net.solarnetwork.common/src/net/solarnetwork/domain/datum/EnergyStorageDatum.java
+++ net.solarnetwork.common/src/net/solarnetwork/domain/datum/EnergyStorageDatum.java
@@ -1,21 +1,21 @@
 /* ==================================================================
  * EnergyStorageDatum.java - 16/02/2016 7:37:54 pm
- * 
+ *
  * Copyright 2007-2016 SolarNetwork.net Dev Team
- * 
- * This program is free software; you can redistribute it and/or 
- * modify it under the terms of the GNU General Public License as 
- * published by the Free Software Foundation; either version 2 of 
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
  * the License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful, 
- * but WITHOUT ANY WARRANTY; without even the implied warranty of 
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License 
- * along with this program; if not, write to the Free Software 
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
  * 02111-1307 USA
  * ==================================================================
  */
@@ -27,33 +27,24 @@ import static net.solarnetwork.domain.datum.DatumSamplesType.Status;
 
 /**
  * Standardized API for energy storage system related datum to implement.
- * 
+ *
  * @author matt
- * @version 2.1
+ * @version 3.0
  */
 public interface EnergyStorageDatum extends Datum {
 
 	/**
 	 * An instantaneous sample key for {@link #getAvailableEnergyPercentage()}
 	 * values.
-	 * 
+	 *
 	 * @since 2.1
 	 */
 	String STATE_OF_CHARGE_PERCENTAGE_KEY = "soc";
 
 	/**
-	 * An instantaneous sample key for {@link #getAvailableEnergyPercentage()}
-	 * values.
-	 * 
-	 * @deprecated since 2.1 use {@link #STATE_OF_CHARGE_PERCENTAGE_KEY}
-	 */
-	@Deprecated
-	String PERCENTAGE_KEY = "percent";
-
-	/**
 	 * An instantaneous sample key for {@link #getStateOfHealthPercentage()}
 	 * values.
-	 * 
+	 *
 	 * @since 2.1
 	 */
 	String STATE_OF_HEALTH_PERCENTAGE_KEY = "soh";
@@ -65,19 +56,19 @@ public interface EnergyStorageDatum extends Datum {
 
 	/**
 	 * An status sample key for {@link #getEnergyCapacity()} values.
-	 * 
+	 *
 	 * @since 2.1
 	 */
 	String CAPACITY_WATT_HOURS_KEY = "capacityWattHours";
 
 	/**
 	 * Get the percentage of energy capacity available in the storage.
-	 * 
+	 *
 	 * <p>
 	 * The {@link #getAvailableEnergy()} value divided by this value, represents
 	 * the total energy capacity of the storage.
 	 * </p>
-	 * 
+	 *
 	 * @return the available energy as a percentage of the total capacity of the
 	 *         storage
 	 */
@@ -89,7 +80,7 @@ public interface EnergyStorageDatum extends Datum {
 	 * Get a percentage of storage "health" in terms of practical total capacity
 	 * right now versus theoretical total capacity when the storage was
 	 * manufactured.
-	 * 
+	 *
 	 * @return the total energy capacity now as a percentage of the theoretical
 	 *         total capacity of the storage when manufactured
 	 * @since 2.1
@@ -100,7 +91,7 @@ public interface EnergyStorageDatum extends Datum {
 
 	/**
 	 * Get the available energy of the storage system, in Wh.
-	 * 
+	 *
 	 * @return the available energy of the storage
 	 */
 	default Long getAvailableEnergy() {
@@ -109,7 +100,7 @@ public interface EnergyStorageDatum extends Datum {
 
 	/**
 	 * Get the energy capacity of the storage system, in Wh.
-	 * 
+	 *
 	 * @return the energy capacity of the storage
 	 * @since 2.1
 	 */
