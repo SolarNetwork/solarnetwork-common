6a85964b50732558b291cc7e8326f57c0b0d066b
Matt Magoffin <matt@solarnetwork.net>
2025-06-10 09:07:56 +1200

    Update to Jetty 12.

--- net.solarnetwork.common.internal.test/src/net/solarnetwork/io/test/UrlUtilsTests.java
+++ net.solarnetwork.common.internal.test/src/net/solarnetwork/io/test/UrlUtilsTests.java
@@ -1,21 +1,21 @@
 /* ==================================================================
  * UrlUtilsTests.java - 11/07/2023 8:07:17 am
- * 
+ *
  * Copyright 2023 SolarNetwork.net Dev Team
- * 
- * This program is free software; you can redistribute it and/or 
- * modify it under the terms of the GNU General Public License as 
- * published by the Free Software Foundation; either version 2 of 
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
  * the License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful, 
- * but WITHOUT ANY WARRANTY; without even the implied warranty of 
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License 
- * along with this program; if not, write to the Free Software 
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
  * 02111-1307 USA
  * ==================================================================
  */
@@ -33,8 +33,9 @@ import java.net.URLConnection;
 import java.nio.charset.StandardCharsets;
 import java.util.zip.GZIPInputStream;
 import java.util.zip.InflaterInputStream;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import org.eclipse.jetty.server.Request;
+import org.eclipse.jetty.server.Response;
+import org.eclipse.jetty.util.Callback;
 import org.junit.Test;
 import org.springframework.util.FileCopyUtils;
 import org.springframework.util.StreamUtils;
@@ -44,7 +45,7 @@ import net.solarnetwork.test.internal.TestHttpHandler;
 
 /**
  * Test cases for the {@link UrlUtils} class.
- * 
+ *
  * @author matt
  * @version 1.0
  */
@@ -58,16 +59,15 @@ public class UrlUtilsTests extends AbstractHttpClientTests {
 		TestHttpHandler handler = new TestHttpHandler() {
 
 			@Override
-			protected boolean handleInternal(HttpServletRequest request, HttpServletResponse response)
+			protected boolean handleInternal(Request request, Response response, Callback callback)
 					throws Exception {
-				response.setHeader("Content-Encoding", "gzip");
-				respondWithJson(response, json);
-				response.flushBuffer();
+				response.getHeaders().put("Content-Encoding", "gzip");
+				respondWithJson(request, response, json);
 				return true;
 			}
 
 		};
-		getHttpServer().addHandler(handler);
+		addHandler(handler);
 
 		// WHEN
 		URLConnection conn = UrlUtils.getURL(log, getHttpServerBaseUrl(), "application/json", null,
@@ -87,16 +87,15 @@ public class UrlUtilsTests extends AbstractHttpClientTests {
 		TestHttpHandler handler = new TestHttpHandler() {
 
 			@Override
-			protected boolean handleInternal(HttpServletRequest request, HttpServletResponse response)
+			protected boolean handleInternal(Request request, Response response, Callback callback)
 					throws Exception {
-				response.setHeader("Content-Encoding", "deflate");
-				respondWithJson(response, json);
-				response.flushBuffer();
+				response.getHeaders().put("Content-Encoding", "deflate");
+				respondWithJson(request, response, json);
 				return true;
 			}
 
 		};
-		getHttpServer().addHandler(handler);
+		addHandler(handler);
 
 		// WHEN
 		URLConnection conn = UrlUtils.getURL(log, getHttpServerBaseUrl(), "application/json", null,
--- net.solarnetwork.common.internal.test/src/net/solarnetwork/test/internal/AbstractHttpClientTests.java
+++ net.solarnetwork.common.internal.test/src/net/solarnetwork/test/internal/AbstractHttpClientTests.java
@@ -1,57 +1,83 @@
 /* ==================================================================
  * AbstractHttpClientTests.java - 19/05/2017 4:10:22 PM
- * 
+ *
  * Copyright 2017 SolarNetwork.net Dev Team
- * 
- * This program is free software; you can redistribute it and/or 
- * modify it under the terms of the GNU General Public License as 
- * published by the Free Software Foundation; either version 2 of 
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
  * the License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful, 
- * but WITHOUT ANY WARRANTY; without even the implied warranty of 
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License 
- * along with this program; if not, write to the Free Software 
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
  * 02111-1307 USA
  * ==================================================================
  */
 
 package net.solarnetwork.test.internal;
 
+import org.eclipse.jetty.server.Handler;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.ServerConnector;
 import org.junit.After;
 import org.junit.Before;
-import org.mortbay.jetty.Connector;
-import org.mortbay.jetty.Server;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
  * Support for HTTP server based tests.
- * 
+ *
  * @author matt
- * @version 1.0
+ * @version 2.0
  */
 public abstract class AbstractHttpClientTests {
 
 	private Server httpServer;
 	private int httpServerPort;
 	private String httpServerBaseUrl;
+	private Handler.Sequence httpServerHandlers;
 
 	protected final Logger log = LoggerFactory.getLogger(getClass());
 
 	@Before
 	public void setup() throws Exception {
 		httpServer = new Server(0);
+		httpServerHandlers = new Handler.Sequence();
+		httpServer.setHandler(httpServerHandlers);
 		httpServer.start();
 
-		Connector c = httpServer.getConnectors()[0];
+		ServerConnector c = (ServerConnector) httpServer.getConnectors()[0];
 		httpServerPort = c.getLocalPort();
 
 		httpServerBaseUrl = "http://localhost:" + c.getLocalPort();
+	}
+
+	/**
+	 * Register a handler.
+	 *
+	 * @param handler
+	 *        the handler to register
+	 * @since 2.0
+	 */
+	protected void addHandler(Handler handler) {
+		httpServerHandlers.addHandler(handler);
+	}
+
+	/**
+	 * Unregister a handler.
+	 *
+	 * @param handler
+	 *        the handler to unregister
+	 * @since 2.0
+	 */
+	protected void removeHandler(Handler handler) {
+		httpServerHandlers.removeHandler(handler);
 	}
 
 	@After
@@ -60,17 +86,8 @@ public abstract class AbstractHttpClientTests {
 	}
 
 	/**
-	 * Get the internal HTTP server.
-	 * 
-	 * @return The HTTP server.
-	 */
-	protected Server getHttpServer() {
-		return httpServer;
-	}
-
-	/**
 	 * Get the port the HTTP server is listening on.
-	 * 
+	 *
 	 * @return the HTTP server port
 	 */
 	protected int getHttpServerPort() {
@@ -79,7 +96,7 @@ public abstract class AbstractHttpClientTests {
 
 	/**
 	 * Get a "base URL" for connecting to the HTTP server.
-	 * 
+	 *
 	 * @return the base URL (without any path) to the HTTP server
 	 */
 	protected String getHttpServerBaseUrl() {
