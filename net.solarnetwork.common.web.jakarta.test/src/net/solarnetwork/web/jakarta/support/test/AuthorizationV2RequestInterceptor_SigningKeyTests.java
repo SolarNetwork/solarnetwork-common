386203d92038f1536667f41acf8487ab761a90cb
Matt Magoffin <matt@solarnetwork.net>
2025-06-22 17:26:19 +1200

    Fix broken tests.

--- net.solarnetwork.common.web.jakarta.test/src/net/solarnetwork/web/jakarta/support/test/AuthorizationV2RequestInterceptor_SigningKeyTests.java
+++ net.solarnetwork.common.web.jakarta.test/src/net/solarnetwork/web/jakarta/support/test/AuthorizationV2RequestInterceptor_SigningKeyTests.java
@@ -1,21 +1,21 @@
 /* ==================================================================
  * AuthorizationV2RequestInterceptorTests.java - 13/08/2019 10:54:17 am
- * 
+ *
  * Copyright 2019 SolarNetwork.net Dev Team
- * 
- * This program is free software; you can redistribute it and/or 
- * modify it under the terms of the GNU General Public License as 
- * published by the Free Software Foundation; either version 2 of 
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
  * the License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful, 
- * but WITHOUT ANY WARRANTY; without even the implied warranty of 
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License 
- * along with this program; if not, write to the Free Software 
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
  * 02111-1307 USA
  * ==================================================================
  */
@@ -23,11 +23,11 @@
 package net.solarnetwork.web.jakarta.support.test;
 
 import static java.util.UUID.randomUUID;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.hasEntry;
 import static org.hamcrest.Matchers.notNullValue;
 import static org.hamcrest.Matchers.startsWith;
-import static org.hamcrest.MatcherAssert.assertThat;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.header;
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.method;
@@ -63,7 +63,7 @@ import net.solarnetwork.web.jakarta.support.StaticAuthorizationCredentialsProvid
 
 /**
  * Test cases for the {@link AuthorizationV2RequestInterceptor} class.
- * 
+ *
  * @author matt
  * @version 1.0
  */
@@ -123,7 +123,7 @@ public class AuthorizationV2RequestInterceptor_SigningKeyTests {
 	    server.expect(requestTo(startsWith(TEST_BASE_URL + "/foo/bar")))
 	        .andExpect(method(HttpMethod.GET))
 	        .andExpect(header(HttpHeaders.HOST, "localhost"))
-	        .andExpect(header(HttpHeaders.AUTHORIZATION, 
+	        .andExpect(header(HttpHeaders.AUTHORIZATION,
 	            startsWith("SNWS2 Credential=" + credentialsProvider.getAuthorizationId() + ",SignedHeaders=accept;date;host,Signature=")))
 	        .andRespond(withSuccess(responseBody, APPLICATION_JSON).headers(respHeaders));
 	    // @formatter:on
@@ -155,15 +155,15 @@ public class AuthorizationV2RequestInterceptor_SigningKeyTests {
 				.method(HttpMethod.POST.toString())
 				.host(TEST_HOST)
 				.path("/foo/bar")
-				.contentType(MediaType.APPLICATION_FORM_URLENCODED + ";charset=UTF-8")
+				.contentType(MediaType.APPLICATION_FORM_URLENCODED.toString())
 				.date(Instant.ofEpochMilli(reqDate))
 				.header("Accept", "application/json, application/*+json")
 				.header("Content-Length", "9")
 				.parameterMap(Collections.singletonMap("topic", new String[] { "foo" }));
-		
+
 		log.debug("Canonical req data:\n{}", auth.computeCanonicalRequestMessage());
 		log.debug("Signature data:\n{}", auth.computeSignatureData(Instant.ofEpochMilli(reqDate), auth.computeCanonicalRequestMessage()));
-		
+
 		final String authHeader = auth.build();
 
 	    server.expect(requestTo(startsWith(TEST_BASE_URL + "/foo/bar")))
