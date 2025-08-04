0cc6c65fcd790e244d6bf9865e76604ded9d9757
Matt Magoffin <matt@solarnetwork.net>
2025-08-05 07:58:26 +1200

    Add support for dynamic HTTP logger names based on request URI.
    This allows logging to be more fine-grained, for example to trace HTTP
    exchanges for specific hosts only.

---
+++ net.solarnetwork.common.web.jakarta.test/src/net/solarnetwork/web/jakarta/support/test/LoggingHttpRequestInterceptorTests.java
@@ -0,0 +1,241 @@
+/* ==================================================================
+ * LoggingHttpRequestInterceptorTests.java - 5/08/2025 6:07:52 am
+ *
+ * Copyright 2025 SolarNetwork.net Dev Team
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ * 02111-1307 USA
+ * ==================================================================
+ */
+
+package net.solarnetwork.web.jakarta.support.test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.equalToIgnoringWhiteSpace;
+import static org.hamcrest.Matchers.hasKey;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+import org.eclipse.jetty.server.Request;
+import org.eclipse.jetty.server.Response;
+import org.eclipse.jetty.util.Callback;
+import org.junit.Test;
+import org.slf4j.Marker;
+import org.slf4j.event.Level;
+import org.slf4j.helpers.LegacyAbstractLogger;
+import org.springframework.http.client.BufferingClientHttpRequestFactory;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+import net.solarnetwork.test.http.AbstractHttpServerTests;
+import net.solarnetwork.test.http.TestHttpHandler;
+import net.solarnetwork.web.jakarta.support.LoggingHttpRequestInterceptor;
+
+/**
+ * Test cases for the {@link LoggingHttpRequestInterceptor} class.
+ *
+ * @author matt
+ * @version 1.0
+ */
+public class LoggingHttpRequestInterceptorTests extends AbstractHttpServerTests {
+
+	private RestTemplate restTemplate(LoggingHttpRequestInterceptor interceptor) {
+		RestTemplate debugTemplate = new RestTemplate(
+				new BufferingClientHttpRequestFactory(new SimpleClientHttpRequestFactory()));
+		debugTemplate.setInterceptors(List.of(interceptor));
+		return debugTemplate;
+	}
+
+	private record LogEvent(Level level, String message, Object[] args, Throwable throwable) {
+
+	}
+
+	private static class InMemoryLogger extends LegacyAbstractLogger {
+
+		private static final long serialVersionUID = -7467060751423930571L;
+
+		private final List<LogEvent> events = new ArrayList<>();
+
+		private InMemoryLogger(String name) {
+			super();
+			this.name = name;
+		}
+
+		@Override
+		public boolean isTraceEnabled() {
+			return true;
+		}
+
+		@Override
+		public boolean isDebugEnabled() {
+			return true;
+		}
+
+		@Override
+		public boolean isInfoEnabled() {
+			return true;
+		}
+
+		@Override
+		public boolean isWarnEnabled() {
+			return true;
+		}
+
+		@Override
+		public boolean isErrorEnabled() {
+			return true;
+		}
+
+		@Override
+		protected String getFullyQualifiedCallerName() {
+			return "";
+		}
+
+		@Override
+		protected void handleNormalizedLoggingCall(Level level, Marker marker, String messagePattern,
+				Object[] arguments, Throwable throwable) {
+			events.add(new LogEvent(level, messagePattern, arguments, throwable));
+		}
+
+	}
+
+	private static String dynamicRequestLoggerName(String dynamicLoggerName) {
+		return "net.solarnetwork.http.REQ" + dynamicLoggerName;
+	}
+
+	private static String dynamicResponseLoggerName(String dynamicLoggerName) {
+		return "net.solarnetwork.http.RES" + dynamicLoggerName;
+	}
+
+	@Test
+	public void get_staticPath() {
+		// GIVEN
+		final String responseText = UUID.randomUUID().toString();
+		TestHttpHandler handler = new TestHttpHandler() {
+
+			@Override
+			protected boolean handleInternal(Request request, Response response, Callback callback)
+					throws Exception {
+				respondWithText(request, response, responseText);
+				return true;
+			}
+
+		};
+		addHandler(handler);
+
+		final Map<String, InMemoryLogger> loggers = new ConcurrentHashMap<>(2);
+		final String requestId = UUID.randomUUID().toString();
+
+		final var service = new LoggingHttpRequestInterceptor(false,
+				(logName) -> loggers.computeIfAbsent(logName, (name) -> new InMemoryLogger(name)),
+				(request) -> requestId);
+		final var rest = restTemplate(service);
+
+		// WHEN
+		final URI uri = URI.create(getHttpServerBaseUrl() + "/foo/bar?a=b");
+		final String result = rest.getForObject(uri, String.class);
+
+		// THEN
+		assertThat("Result returned from server", result, is(equalTo(responseText)));
+		assertThat("No dynamic loggers created for request/response", loggers.keySet(), hasSize(0));
+	}
+
+	@Test
+	public void get_dynamicPath() {
+		// GIVEN
+		final String responseText = UUID.randomUUID().toString();
+		TestHttpHandler handler = new TestHttpHandler() {
+
+			@Override
+			protected boolean handleInternal(Request request, Response response, Callback callback)
+					throws Exception {
+				respondWithText(request, response, responseText);
+				return true;
+			}
+
+		};
+		addHandler(handler);
+
+		final Map<String, InMemoryLogger> loggers = new ConcurrentHashMap<>(2);
+		final String requestId = UUID.randomUUID().toString();
+
+		final var service = new LoggingHttpRequestInterceptor(true,
+				(logName) -> loggers.computeIfAbsent(logName, (name) -> new InMemoryLogger(name)),
+				(request) -> requestId);
+		final var rest = restTemplate(service);
+
+		// WHEN
+		final URI uri = URI.create(getHttpServerBaseUrl() + "/foo/bar?a=b");
+		final String result = rest.getForObject(uri, String.class);
+
+		try {
+			Thread.sleep(200L);
+		} catch ( InterruptedException e ) {
+			// continue
+		}
+
+		// THEN
+		assertThat("Result returned from server", result, is(equalTo(responseText)));
+		assertThat("Dynamic loggers created for request/response", loggers.keySet(), hasSize(2));
+
+		final String expectedDynamicLoggerName = ".localhost." + getHttpServerPort() + ".foo.bar";
+		final String expectedRequestLoggerName = dynamicRequestLoggerName(expectedDynamicLoggerName);
+		final String expectedResponseLoggerName = dynamicResponseLoggerName(expectedDynamicLoggerName);
+		assertThat("Dynamic request logger created for host, port, path", loggers,
+				hasKey(expectedRequestLoggerName));
+		final InMemoryLogger requestLogger = loggers.get(expectedRequestLoggerName);
+		assertThat("Request log event created", requestLogger.events, hasSize(1));
+		final LogEvent requestEvent = requestLogger.events.get(0);
+		assertThat("Request log event is TRACE level", requestEvent.level, is(equalTo(Level.TRACE)));
+		assertThat("Request log message", requestEvent.message, is(equalToIgnoringWhiteSpace("""
+				Begin request to: http://localhost:%d/foo/bar?a=b
+				>>>>>>>>>> request begin %s >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+				GET /foo/bar?a=b
+				Host: localhost:%1$d
+				Accept: text/plain, application/json, application/*+json, */*
+				Content-Length: 0
+
+
+				>>>>>>>>>> request end   %2$s >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+				""".formatted(getHttpServerPort(), requestId))));
+
+		assertThat("Dynamic response logger created for host, port, path", loggers,
+				hasKey(expectedResponseLoggerName));
+		final InMemoryLogger responseLogger = loggers.get(expectedResponseLoggerName);
+		assertThat("Response log event created", responseLogger.events, hasSize(1));
+		final LogEvent responseEvent = responseLogger.events.get(0);
+		assertThat("Response log event is TRACE level", responseEvent.level, is(equalTo(Level.TRACE)));
+		assertThat("Response log message",
+				responseEvent.message.replaceAll("Date:[ a-zA-Z0-9:,-]+\\n", "Date: -----\n"),
+				is(equalToIgnoringWhiteSpace("""
+						Request response:
+						<<<<<<<<<< response begin %s <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+						200 OK
+						Server: Jetty(12.0.22)
+						Date: -----
+						Content-Type: text/plain; charset=utf-8
+						Transfer-Encoding: chunked
+
+						%s
+						<<<<<<<<<< response end   %1$s <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+						""".formatted(requestId, responseText))));
+	}
+
+}
--- net.solarnetwork.common.web.jakarta/src/net/solarnetwork/web/jakarta/support/LoggingHttpRequestInterceptor.java
+++ net.solarnetwork.common.web.jakarta/src/net/solarnetwork/web/jakarta/support/LoggingHttpRequestInterceptor.java
@@ -1,21 +1,21 @@
 /* ==================================================================
  * LoggingHttpRequestInterceptor.java - 24/08/2017 6:17:31 AM
- * 
+ *
  * Copyright 2017 SolarNetwork.net Dev Team
- * 
- * This program is free software; you can redistribute it and/or 
- * modify it under the terms of the GNU General Public License as 
- * published by the Free Software Foundation; either version 2 of 
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
  * the License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful, 
- * but WITHOUT ANY WARRANTY; without even the implied warranty of 
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License 
- * along with this program; if not, write to the Free Software 
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
  * 02111-1307 USA
  * ==================================================================
  */
@@ -33,15 +33,18 @@ import java.io.Writer;
 import java.net.URI;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
+import java.util.function.Function;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.zip.DeflaterInputStream;
 import java.util.zip.GZIPInputStream;
 import org.apache.commons.codec.binary.Hex;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.PropertyAccessorFactory;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpRequest;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.MediaType;
 import org.springframework.http.client.BufferingClientHttpRequestFactory;
 import org.springframework.http.client.ClientHttpRequestExecution;
@@ -54,63 +57,197 @@ import org.springframework.web.client.HttpStatusCodeException;
 
 /**
  * A client HTTP interceptor for logging the full details of each request.
- * 
+ *
  * <p>
  * Request messages are logged at {@literal TRACE} level under the
  * {@literal net.solarnetwork.http.REQ} logger; responses under
- * {@literal net.solarnetwork.http.RES}.
+ * {@literal net.solarnetwork.http.RES}. If {@code dynamicUrlLoggers} is set to
+ * {@code true} then URL components will be added as logger components:
  * </p>
- * 
+ *
+ * <ol>
+ * <li>the host name</li>
+ * <li>the port</li>
+ * <li>the path, with {@code /} characters replaced by {@code .}</li>
+ * </ol>
+ *
  * @author matt
- * @version 1.2
+ * @version 1.3
  */
 public class LoggingHttpRequestInterceptor implements ClientHttpRequestInterceptor {
 
-	private static final Logger REQ_LOG = LoggerFactory.getLogger("net.solarnetwork.http.REQ");
-	private static final Logger RES_LOG = LoggerFactory.getLogger("net.solarnetwork.http.RES");
+	/**
+	 * The static request log name, or dynamic request log prefix.
+	 *
+	 * @since 1.3
+	 */
+	public static final String REQUEST_LOG_NAME = "net.solarnetwork.http.REQ";
+
+	/**
+	 * The static response log name, or dynamic response log prefix.
+	 *
+	 * @since 1.4
+	 */
+	public static final String RESPONSE_LOG_NAME = "net.solarnetwork.http.RES";
+
+	private static final Logger REQ_LOG = LoggerFactory.getLogger(REQUEST_LOG_NAME);
+	private static final Logger RES_LOG = LoggerFactory.getLogger(RESPONSE_LOG_NAME);
+
+	private static final Pattern PATH_SPLIT_REGEX = Pattern.compile("/");
+
+	private final Function<String, Logger> dynamicLoggerProvider;
+	private final Function<HttpRequest, String> requestIdProvider;
+	private final boolean dynamicUrlLoggers;
 
 	/**
 	 * Constructor.
 	 */
 	public LoggingHttpRequestInterceptor() {
+		this(false);
+	}
+
+	/**
+	 * Constructor.
+	 *
+	 * @param dynamicUrlLoggers
+	 *        {@code true} to dynamically add URL components to the resolved
+	 *        request/response loggers
+	 * @since 1.3
+	 */
+	public LoggingHttpRequestInterceptor(final boolean dynamicUrlLoggers) {
+		this(dynamicUrlLoggers, null, null);
+	}
+
+	/**
+	 * Constructor.
+	 *
+	 * @param dynamicUrlLoggers
+	 *        {@code true} to dynamically add URL components to the resolved
+	 *        request/response loggers
+	 * @param dynamicLoggerProvider
+	 *        function to supply dynamic loggers, or {@code null} for default
+	 *        provider
+	 * @param requestIdProvider
+	 *        the request ID provider, or {@code null} for a default (random)
+	 *        provider
+	 * @since 1.3
+	 */
+	public LoggingHttpRequestInterceptor(final boolean dynamicUrlLoggers,
+			final Function<String, Logger> dynamicLoggerProvider,
+			Function<HttpRequest, String> requestIdProvider) {
 		super();
+		this.dynamicUrlLoggers = dynamicUrlLoggers;
+		this.dynamicLoggerProvider = dynamicLoggerProvider != null ? dynamicLoggerProvider
+				: LoggerFactory::getLogger;
+		this.requestIdProvider = requestIdProvider != null ? requestIdProvider
+				: LoggingHttpRequestInterceptor::randomRequestId;
+	}
+
+	/**
+	 * The default request ID provider.
+	 *
+	 * <p>
+	 * This implementation returns a random UUID string.
+	 * </p>
+	 *
+	 * @param request
+	 *        the HTTP request
+	 * @return the random ID
+	 * @since 1.3
+	 */
+	public static String randomRequestId(HttpRequest request) {
+		return UUID.randomUUID().toString();
 	}
 
 	/**
 	 * Get a client request factory with logging support if either request or
 	 * response logging is enabled.
-	 * 
+	 *
 	 * <p>
 	 * This method will check if either {@code REQ_LOG} or {@code RES_LOG} have
 	 * {@literal TRACE} level logging enabled, and if so return a
 	 * {@link BufferingClientHttpRequestFactory} suitable for logging. Otherwise
 	 * the default {@link SimpleClientHttpRequestFactory} is returned.
-	 * 
+	 * </p>
+	 *
 	 * @return a new request factory
 	 */
 	public static ClientHttpRequestFactory requestFactory() {
-		return requestFactory(new SimpleClientHttpRequestFactory());
+		return requestFactory(false, null);
+	}
+
+	/**
+	 * Get a client request factory with logging support if either request or
+	 * response logging is enabled.
+	 *
+	 * <p>
+	 * This method will check if either {@code REQ_LOG} or {@code RES_LOG} have
+	 * {@literal TRACE} level logging enabled, and if so return a
+	 * {@link BufferingClientHttpRequestFactory} suitable for logging. Otherwise
+	 * the default {@link SimpleClientHttpRequestFactory} is returned.
+	 * </p>
+	 *
+	 * @param dynamicUrlLoggers
+	 *        {@code true} to dynamically add URL components to the resolved
+	 *        request/response loggers
+	 * @param dynamicLoggerProvider
+	 *        function to supply dynamic loggers, or {@code null} for default
+	 *        provider
+	 * @return a new request factory
+	 * @since 1.3
+	 */
+	public static ClientHttpRequestFactory requestFactory(final boolean dynamicUrlLoggers,
+			final Function<String, Logger> dynamicLoggerProvider) {
+		return requestFactory(new SimpleClientHttpRequestFactory(), dynamicUrlLoggers);
 	}
 
 	/**
 	 * Wrap a client request factory with logging support if either request or
 	 * response logging is enabled.
-	 * 
+	 *
 	 * <p>
 	 * This method will check if either {@code REQ_LOG} or {@code RES_LOG} have
 	 * {@literal TRACE} level logging enabled, and if so return a
 	 * {@link BufferingClientHttpRequestFactory} suitable for logging. Otherwise
 	 * {@code requestFactory} is returned directly.
 	 * </p>
-	 * 
+	 *
 	 * @param requestFactory
 	 *        request factory to possibly wrap
 	 * @return the request factory
 	 * @since 1.1
 	 */
-	public static ClientHttpRequestFactory requestFactory(ClientHttpRequestFactory requestFactory) {
+	public static ClientHttpRequestFactory requestFactory(
+			final ClientHttpRequestFactory requestFactory) {
+		return requestFactory(requestFactory, false);
+	}
+
+	/**
+	 * Wrap a client request factory with logging support if either request or
+	 * response logging is enabled.
+	 *
+	 * <p>
+	 * This method will check if either {@code REQ_LOG} or {@code RES_LOG} have
+	 * {@code TRACE} (if {@code dynamiceUrlLoggers} is {@code false}) or
+	 * {@code DEBUG} (if {@code dynamicUrlLoggers} is {@code true}) level
+	 * logging enabled, and if so return a
+	 * {@link BufferingClientHttpRequestFactory} suitable for logging. Otherwise
+	 * {@code requestFactory} is returned directly.
+	 * </p>
+	 *
+	 * @param requestFactory
+	 *        request factory to possibly wrap
+	 * @param dynamicUrlLoggers
+	 *        {@code true} to dynamically add URL components to the resolved
+	 *        request/response loggers
+	 * @return the request factory
+	 * @since 1.3
+	 */
+	public static ClientHttpRequestFactory requestFactory(final ClientHttpRequestFactory requestFactory,
+			final boolean dynamicUrlLoggers) {
 		ClientHttpRequestFactory reqFactory = requestFactory;
-		if ( REQ_LOG.isTraceEnabled() || RES_LOG.isTraceEnabled() ) {
+		if ( (dynamicUrlLoggers && (REQ_LOG.isDebugEnabled() || RES_LOG.isDebugEnabled()))
+				|| (!dynamicUrlLoggers && (REQ_LOG.isTraceEnabled() || RES_LOG.isTraceEnabled())) ) {
 			reqFactory = new BufferingClientHttpRequestFactory(requestFactory);
 		}
 		return reqFactory;
@@ -119,49 +256,90 @@ public class LoggingHttpRequestInterceptor implements ClientHttpRequestIntercept
 	/**
 	 * Test if a {@link ClientHttpRequestFactory} supports logging with this
 	 * interceptor.
-	 * 
+	 *
 	 * @param reqFactory
 	 *        the request factory to test
 	 * @return {@literal true} if the factory is a
 	 *         {@link BufferingClientHttpRequestFactory}
 	 */
-	public static boolean supportsLogging(ClientHttpRequestFactory reqFactory) {
+	public static boolean supportsLogging(final ClientHttpRequestFactory reqFactory) {
 		return (reqFactory instanceof BufferingClientHttpRequestFactory);
+	}
+
+	private String dynamicLoggerName(URI uri) {
+		if ( !dynamicUrlLoggers ) {
+			return null;
+		}
+
+		StringBuilder buf = new StringBuilder();
+
+		String comp = uri.getHost();
+		if ( comp != null && !comp.isEmpty() ) {
+			buf.append('.');
+			buf.append(comp);
+		}
+
+		comp = String.valueOf(uri.getPort());
+		if ( buf.charAt(buf.length() - 1) != '.' ) {
+			buf.append('.');
+			buf.append(comp);
+		}
+
+		comp = uri.getPath();
+		if ( comp != null && !comp.isBlank() ) {
+			String[] comps = PATH_SPLIT_REGEX.split(comp, 0);
+			for ( String c : comps ) {
+				if ( !c.isEmpty() ) {
+					buf.append('.');
+					buf.append(c);
+				}
+			}
+		}
+		return buf.toString();
 	}
 
 	@Override
 	public ClientHttpResponse intercept(HttpRequest request, byte[] body,
 			ClientHttpRequestExecution execution) throws IOException {
-		if ( REQ_LOG.isTraceEnabled() ) {
-			traceRequest(request, body);
+		final String dynamicLoggerName = dynamicLoggerName(request.getURI());
+		final Logger requestLog = dynamicLoggerName != null
+				? dynamicLoggerProvider.apply(REQUEST_LOG_NAME + dynamicLoggerName)
+				: REQ_LOG;
+		final Logger responseLog = dynamicLoggerName != null
+				? dynamicLoggerProvider.apply(RESPONSE_LOG_NAME + dynamicLoggerName)
+				: RES_LOG;
+		final String exchangeId = requestIdProvider.apply(request);
+		if ( requestLog.isTraceEnabled() ) {
+			traceRequest(requestLog, exchangeId, request, body);
 		}
 		try {
 			ClientHttpResponse response = execution.execute(request, body);
-			if ( RES_LOG.isTraceEnabled() ) {
-				traceResponse(response);
+			if ( responseLog.isTraceEnabled() ) {
+				traceResponse(responseLog, exchangeId, response);
 			}
 			return response;
 		} catch ( HttpStatusCodeException e ) {
 			// reflection-access to work with Spring 5 and 6, where the method signature changed
-			Object statusCode = PropertyAccessorFactory.forBeanPropertyAccess(e)
-					.getPropertyValue("statusCode");
-			traceResponse(statusCode, e.getStatusText(), e.getResponseHeaders(),
+			HttpStatusCode statusCode = e.getStatusCode();
+			traceResponse(responseLog, exchangeId, statusCode, e.getStatusText(), e.getResponseHeaders(),
 					new ByteArrayInputStream(e.getResponseBodyAsByteArray()), null);
 			throw e;
 		} catch ( IOException e ) {
-			traceResponse(e);
+			traceResponse(responseLog, exchangeId, e);
 			throw e;
 		} catch ( RuntimeException e ) {
-			traceResponse(e);
+			traceResponse(responseLog, exchangeId, e);
 			throw e;
 		}
 	}
 
-	private void traceRequest(HttpRequest request, byte[] body) throws IOException {
+	private void traceRequest(final Logger log, final String exchangeId, final HttpRequest request,
+			final byte[] body) throws IOException {
 		URI uri = request.getURI();
 		StringBuilder buf = new StringBuilder("Begin request to: ");
 		buf.append(uri).append('\n');
-		buf.append(">>>>>>>>>> request begin >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n");
+		buf.append(">>>>>>>>>> request begin ").append(exchangeId)
+				.append(" >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n");
 		buf.append(request.getMethod()).append(' ').append(uri.getPath());
 		if ( uri.getRawQuery() != null ) {
 			buf.append('?').append(uri.getRawQuery());
@@ -178,31 +356,33 @@ public class LoggingHttpRequestInterceptor implements ClientHttpRequestIntercept
 		}
 		buf.append('\n');
 		buf.append(new String(body, "UTF-8")).append('\n');
-		buf.append(">>>>>>>>>> request end   >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
-		REQ_LOG.trace(buf.toString());
+		buf.append(">>>>>>>>>> request end   ").append(exchangeId)
+				.append(" >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+		log.trace(buf.toString());
 	}
 
-	private void traceResponse(Throwable e) {
+	private void traceResponse(Logger log, String exchangeId, Throwable e) {
 		try {
-			traceResponse(null, null, null, null, e);
+			traceResponse(log, exchangeId, null, null, null, null, e);
 		} catch ( Exception ex ) {
-			RES_LOG.trace("{} tracing response", ex.getClass().getName(), ex);
+			log.trace("{} tracing response", ex.getClass().getName(), ex);
 		}
 	}
 
-	private void traceResponse(ClientHttpResponse response) {
+	private void traceResponse(Logger log, String exchangeId, ClientHttpResponse response) {
 		try {
-			traceResponse(response.getStatusCode().value(), response.getStatusText(),
+			traceResponse(log, exchangeId, response.getStatusCode().value(), response.getStatusText(),
 					response.getHeaders(), response.getBody(), null);
 		} catch ( Exception ex ) {
-			RES_LOG.trace("{} tracing response", ex.getClass().getName(), ex);
+			log.trace("{} tracing response", ex.getClass().getName(), ex);
 		}
 	}
 
-	private void traceResponse(Object statusCode, String statusText, HttpHeaders headers, InputStream in,
-			Throwable exception) throws IOException {
+	private void traceResponse(Logger log, String exchangeId, Object statusCode, String statusText,
+			HttpHeaders headers, InputStream in, Throwable exception) throws IOException {
 		final StringBuilder buf = new StringBuilder("Request response:\n");
-		buf.append("<<<<<<<<<< response begin <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\n");
+		buf.append("<<<<<<<<<< response begin ").append(exchangeId)
+				.append(" <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\n");
 		if ( statusCode != null ) {
 			buf.append(statusCode).append(' ').append(statusText).append('\n');
 		}
@@ -219,8 +399,9 @@ public class LoggingHttpRequestInterceptor implements ClientHttpRequestIntercept
 			buf.append('\n');
 			traceResponseBody(headers, in, buf);
 		}
-		buf.append("<<<<<<<<<< response end   <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<");
-		RES_LOG.trace(buf.toString());
+		buf.append("<<<<<<<<<< response end   ").append(exchangeId)
+				.append(" <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<");
+		log.trace(buf.toString());
 	}
 
 	private void traceResponseBody(HttpHeaders headers, InputStream in, final StringBuilder buf)
