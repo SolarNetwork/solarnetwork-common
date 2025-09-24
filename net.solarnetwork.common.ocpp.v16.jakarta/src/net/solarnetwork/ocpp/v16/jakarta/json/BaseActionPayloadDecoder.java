e7c0a4aa7a9e03ed4f4fd4fc9a7846c74cb1f80a
Matt Magoffin <matt@solarnetwork.net>
2025-09-24 16:43:50 +1200

    Fix Jackson 2.20 deprecation.

--- net.solarnetwork.common.ocpp.v16.jakarta/src/net/solarnetwork/ocpp/v16/jakarta/json/BaseActionPayloadDecoder.java
+++ net.solarnetwork.common.ocpp.v16.jakarta/src/net/solarnetwork/ocpp/v16/jakarta/json/BaseActionPayloadDecoder.java
@@ -1,21 +1,21 @@
 /* ==================================================================
  * BaseActionPayloadDecoder.java - 3/02/2020 3:42:44 pm
- * 
+ *
  * Copyright 2020 SolarNetwork.net Dev Team
- * 
- * This program is free software; you can redistribute it and/or 
- * modify it under the terms of the GNU General Public License as 
- * published by the Free Software Foundation; either version 2 of 
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
  * the License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful, 
- * but WITHOUT ANY WARRANTY; without even the implied warranty of 
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License 
- * along with this program; if not, write to the Free Software 
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
  * 02111-1307 USA
  * ==================================================================
  */
@@ -37,9 +37,9 @@ import net.solarnetwork.ocpp.xml.jakarta.support.SchemaValidationHelper;
 /**
  * Abstract base implementation of {@link ActionPayloadDecoder} with JAXB schema
  * validation support.
- * 
+ *
  * @author matt
- * @version 1.0
+ * @version 1.1
  */
 public abstract class BaseActionPayloadDecoder implements ActionPayloadDecoder {
 
@@ -54,7 +54,7 @@ public abstract class BaseActionPayloadDecoder implements ActionPayloadDecoder {
 
 	/**
 	 * Get a default {@link ObjectMapper} instance.
-	 * 
+	 *
 	 * @return a default mapper
 	 */
 	public static ObjectMapper defaultObjectMapper() {
@@ -62,13 +62,13 @@ public abstract class BaseActionPayloadDecoder implements ActionPayloadDecoder {
 		mapper.registerModule(new JakartaXmlBindAnnotationModule());
 		mapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
 		mapper.setDateFormat(new StdDateFormat().withColonInTimeZone(true));
-		mapper.setSerializationInclusion(Include.NON_EMPTY);
+		mapper.setDefaultPropertyInclusion(Include.NON_EMPTY);
 		return mapper;
 	}
 
 	/**
 	 * Constructor.
-	 * 
+	 *
 	 * @param mapper
 	 *        the object mapper to use
 	 * @param jaxbRegistry
