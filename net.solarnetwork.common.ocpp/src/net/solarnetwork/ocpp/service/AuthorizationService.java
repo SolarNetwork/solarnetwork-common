3d167888df505728c70c986eb7ddabb0c8ee5047
Matt Magoffin <matt@solarnetwork.net>
2026-02-26 08:24:29 +1300

    Tweak nullability.

--- net.solarnetwork.common.ocpp/src/net/solarnetwork/ocpp/service/AuthorizationService.java
+++ net.solarnetwork.common.ocpp/src/net/solarnetwork/ocpp/service/AuthorizationService.java
@@ -1,41 +1,42 @@
 /* ==================================================================
  * AuthorizationService.java - 6/02/2020 7:16:07 pm
- * 
+ *
  * Copyright 2020 SolarNetwork.net Dev Team
- * 
- * This program is free software; you can redistribute it and/or 
- * modify it under the terms of the GNU General Public License as 
- * published by the Free Software Foundation; either version 2 of 
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
  * the License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful, 
- * but WITHOUT ANY WARRANTY; without even the implied warranty of 
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License 
- * along with this program; if not, write to the Free Software 
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
  * 02111-1307 USA
  * ==================================================================
  */
 
 package net.solarnetwork.ocpp.service;
 
+import org.jspecify.annotations.Nullable;
 import net.solarnetwork.ocpp.domain.AuthorizationInfo;
 import net.solarnetwork.ocpp.domain.ChargePointIdentity;
 import net.solarnetwork.service.Identifiable;
 
 /**
  * API for authorizing an ID tag.
- * 
+ *
  * <p>
  * This API can be used by a Charge Point to authorize an identifier value, for
  * example against a local authorization list or by making a call to a remote
  * service such as an OCPP Central System. This API can also be used by a
  * Central System to authorize a request from a Charge Point.
  * </p>
- * 
+ *
  * @author matt
  * @version 2.0
  */
@@ -43,13 +44,13 @@ public interface AuthorizationService extends Identifiable {
 
 	/**
 	 * Request authorization of a specific charge point ID tag value.
-	 * 
+	 *
 	 * @param clientId
 	 *        the ID of the client making the request
 	 * @param identifier
 	 *        the identifier to authorize, e.g. RFID value
 	 * @return the authorization result, never {@code null}
 	 */
-	AuthorizationInfo authorize(ChargePointIdentity clientId, String identifier);
+	AuthorizationInfo authorize(@Nullable ChargePointIdentity clientId, String identifier);
 
 }
